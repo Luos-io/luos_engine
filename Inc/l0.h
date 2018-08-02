6e384b870de03ec8524eeaccfcbaa0020f2335c6
Nicolas Rabault <nicolas.rabault@pollen-robotics.com>
2018-08-02 10:47:01 +0200

    Fix temperature and manage voltage measurement

--- Inc/l0.h
+++ Inc/l0.h
@@ -5,13 +5,13 @@
 
 #define TEMP110_CAL_VALUE                                           ((uint16_t*)((uint32_t)0x1FFFF7C2))
 #define TEMP30_CAL_VALUE                                            ((uint16_t*)((uint32_t)0x1FFFF7B8))
-
+#define VOLTAGEFACTOR                                               (10.0f + 68.0f) / 10.0f
  // This structure need to list all ADC configured in the good order determined by the
  // ADC_CHANEL number in increasing order
 typedef struct __attribute__((__packed__)){
      union {
          struct __attribute__((__packed__)){
-             uint32_t power_sensor;
+             uint32_t voltage_sensor;
              uint32_t temperature_sensor;
          };
          uint32_t unmap[2];                /*!< Uncmaped form. */
--- src/l0.c
+++ src/l0.c
@@ -13,9 +13,25 @@ int l0_msg_handler(vm_t* vm, msg_t* input, msg_t* output) {
 	      output->header.target_mode = ID;
 	      output->header.size = sizeof(float);
 	      output->header.target = input->header.source;
-	      float temp = ((110.0f - 30.0f) / ((float)(*TEMP110_CAL_VALUE) - (float)(*TEMP30_CAL_VALUE)) * ((float)L0_analog.temperature_sensor - (float)(*TEMP30_CAL_VALUE)) + 30.0f);
-	      memcpy(input->data, &temp, sizeof(float));
+	      //float temp = ((110.0f - 30.0f) / ((float)(*TEMP110_CAL_VALUE) - (float)(*TEMP30_CAL_VALUE)) * ((float)L0_analog.temperature_sensor - (float)(*TEMP30_CAL_VALUE)) + 30.0f);
+
+		  float temp;
+	      temp = (((float)L0_analog.temperature_sensor * 300.0f / 330.0f) - (float)(*TEMP30_CAL_VALUE) );
+	      temp = temp * (110.0f - 30.0f);
+	      temp = temp /(float)(*TEMP110_CAL_VALUE - *TEMP30_CAL_VALUE);
+	      temp = temp + 30.0f;
+
+	      memcpy(output->data, &temp, sizeof(float));
 	      return L0_TEMPERATURE;
+	   }
+	  if (input->header.cmd == L0_VOLTAGE) {
+	      output->header.cmd = L0_VOLTAGE;
+	      output->header.target_mode = ID;
+	      output->header.size = sizeof(float);
+	      output->header.target = input->header.source;
+	      float volt = (((float)L0_analog.voltage_sensor * 3.3f) / 4096.0f) * VOLTAGEFACTOR;
+	      memcpy(output->data, &volt, sizeof(float));
+	      return L0_VOLTAGE;
 	   }
 	  return LUOS_PROTOCOL_NB;
 }
