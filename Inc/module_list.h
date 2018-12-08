684d974baf7b4f831253f3de579799b74b9ea0b9
Nicolas Rabault <nicolas.rabault@pollen-robotics.com>
2018-12-08 12:22:26 +0100

    add IMU, Light sensor, and controlled motor; type, registers, and structures

--- Inc/module_list.h
+++ Inc/module_list.h
@@ -17,7 +17,10 @@ typedef enum {
   STEPPER,
   DCMOTOR,
   GPIO,
-  HANDY
+  HANDY,
+  IMU,
+  LIGHT,
+  CONTROLLED_MOTOR
 } module_type_t;
 
 typedef enum {
@@ -30,24 +33,48 @@ typedef enum {
 
   // Generic data register
   COLOR, // char[3] (R, G, B)
-  ROTATION_POSITION, // float (°)
-  ROTATION_SPEED, // float (°/s)
   COMPLIANT, // char (True/False)
   IO_STATE, // char (True/False)
-  LINEAR_POSITION, // Float (mm)
-  LINEAR_SPEED, // Float (mm/s)
+  POWER_RATIO, // float (-100.0% <-> 100.0%)
+  PEDOMETER, // long[2] (step number and step time millisecond)
+  LUX, // float (Lux)
+  CURRENT, // float (Ampere)
+
+  // Configuration commands
   REINIT, // char (True/False)
   STOP, // char (True/False)
-  POWER_RATIO, // float (-100.0% <-> 100.0%)
+  PID, // pid_t float[3] = {p, i, d}
+  RESOLUTION, // resolution parameter for a sensor float
+  REDUCTION, // reduction factor (mechanical for example) float
+  DIMENSION, // dimention of an element mm float
+
+  // L0 things
   L0_TEMPERATURE, // float (°c)
   L0_VOLTAGE, // float (V)
   L0_LED, // char (True/False)
 
+  // Space positioning
+  ROTATION_POSITION, // float (°)
+  ROTATION_SPEED, // float (°/s)
+  TRANSLATION_POSITION, // Float (mm)
+  TRANSLATION_SPEED, // Float (mm/s)
+  ACCEL_3D, // long[3](X, Y, Z axis linear acceleration data in Gees)
+  GYRO_3D, // long[3](X, Y, Z axis rotational acceleration data in degrees per second)
+  QUATERNION, // long[4] (sensor fused w, x, y, z rotational angles)
+  COMPASS_3D, // long[3](magnetic field data in micro tesla on each axis)
+  EULER_3D, // long[3](Pitch, roll, yaw based in degrees with frame reference)
+  ROT_MAT, // short[9] (linear math 9 element matrix representation)
+  LINEAR_ACCEL, // float[3] (linear acceleration in body frame coordinates)
+  GRAVITY_VECTOR, // float[3] (Which access gravity effects)
+  HEADING, // long (360 degrees from North with Y+ axis as the pointer)
+
   // Specific register
-  DXL_WHEELMODE, // char (True/False)
+  DXL_WHEELMODE, // char (True/False) TODO => should be managed by MOTOR_REPORT
   GPIO_GET_STATE, // gpio_t
   GPIO_SET_STATE, // char[3] (p2, p3, p4)
   HANDY_SET_POSITION, // handy_t
+  IMU_REPORT, // imu_report_t (enable and disable IMU value type reporting)
+  MOTOR_REPORT, // motor_mode_t (enable and disable motor value type reporting and target mode)
 
   LUOS_PROTOCOL_NB,
 } module_register_t;
--- Inc/module_structs.h
+++ Inc/module_structs.h
@@ -21,16 +21,92 @@ typedef struct __attribute__((__packed__)){
 
  // Handy struct
  typedef struct __attribute__((__packed__)){
-      union {
-          struct __attribute__((__packed__)){
-        	  uint8_t index;
-        	  uint8_t middle;
-        	  uint8_t ring;
-        	  uint8_t pinky;
-        	  uint8_t thumb;
-          };
-          uint8_t unmap[(5 * sizeof(uint8_t))];                /*!< Uncmaped form. */
-      };
-  }handy_t;
+	union {
+		struct __attribute__((__packed__)){
+		  uint8_t index;
+		  uint8_t middle;
+		  uint8_t ring;
+		  uint8_t pinky;
+		  uint8_t thumb;
+		};
+		uint8_t unmap[(5 * sizeof(uint8_t))];                /*!< Uncmaped form. */
+	};
+}handy_t;
+
+// Imu report struct
+typedef struct __attribute__((__packed__)){
+    union {
+        struct __attribute__((__packed__)){
+            unsigned short accell : 1;
+            unsigned short gyro : 1;
+            unsigned short quat : 1;
+            unsigned short compass : 1;
+            unsigned short euler : 1;
+            unsigned short rot_mat : 1;
+            unsigned short pedo : 1;
+            unsigned short linear_accel : 1;
+            unsigned short gravity_vector : 1;
+            unsigned short heading : 1;
+        };
+        unsigned char unmap[2];
+    };
+}imu_report_t;
+
+// Pid
+typedef struct __attribute__((__packed__)){
+    union {
+        struct __attribute__((__packed__)){
+            float p;
+            float i;
+            float d;
+        };
+        unsigned char unmap[3 * sizeof(float)];
+    };
+}asserv_pid_t;
+
+// Motor
+typedef struct __attribute__((__packed__)){
+    union {
+            struct __attribute__((__packed__)){
+                // target modes
+                uint16_t mode_compliant : 1;
+                uint16_t mode_power : 1;
+                uint16_t mode_torque : 1;
+                uint16_t mode_rot_speed : 1;
+                uint16_t mode_rot_position : 1;
+                uint16_t mode_trans_speed : 1;
+                uint16_t mode_trans_position : 1;
+
+                // report modes
+                uint16_t rot_position : 1;
+                uint16_t rot_speed : 1;
+                uint16_t trans_distance : 1;
+                uint16_t trans_speed : 1;
+                uint16_t current : 1;
+            };
+            uint8_t unmap[2];
+    };
+} motor_mode_t;
+
+typedef struct __attribute__((__packed__)){
+        float power;
+
+        // targets
+        motor_mode_t mode;
+        float target_rot_position;
+        float target_rot_speed;
+
+        // measures
+        float rot_position;
+        float rot_speed;
+        float trans_distance;
+        float trans_speed;
+
+        //configs
+        float motor_reduction;
+        float encoder_res;
+        float wheel_diameter_mm;
+}motor_config_t;
+
 
 #endif /*__ __L0_H */
