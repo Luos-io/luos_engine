683dde59d3f78fe15b2bbc9b52160328b21f44a2
Nicolas Rabault <nicolas.rabault@pollen-robotics.com>
2019-06-07 10:15:07 +0200

    refactor API :  - public function is now private  - add all used Robus functions

--- Inc/luos.h
+++ Inc/luos.h
@@ -3,16 +3,52 @@
 
 #include "module_list.h"
 #include "module_structs.h"
-#include "robus.h"
+#include "robus_struct.h"
 
-// Luos unic ID => ARM serial number
-typedef struct __attribute__((__packed__)){
-    union {
-        uint32_t uuid[3];
-        uint8_t unmap[3 * sizeof(uint32_t)];                /*!< Uncmaped form. */
-    };
-}luos_uuid_t;
+/**
+ * \fn void luos_init(void)
+ * \brief Initialisation of Luos.
+ *
+ */
+void luos_init(void);
 
-int luos_msg_handler(vm_t* vm, msg_t* input, msg_t* output);
+/**
+ * \fn void luos_loop(void)
+ * \brief loop of Luos.
+ *
+ */
+void luos_loop(void);
+
+/**
+ * \fn void luos_modules_clear(void)
+ * \brief Completely reset the list of virtual modules.
+ *
+ */
+void luos_modules_clear(void);
+
+/**
+ * \fn vm_t* luos_module_create(RX_CB rx_cb, unsigned char type, unsigned char *alias)
+ * \brief Initialisation of the Robus communication lib.
+ *
+ * \param rx_cb function pointer into the rx callback.
+ * \param type type reference of this module hardware.
+ * \param alias string (15 caracters max).
+ *
+ * \return virtual module object pointer.
+ *
+ */
+vm_t* luos_module_create(RX_CB mod_cb, unsigned char type, const char *alias);
+
+/**
+ * \fn unsigned char luos_send(vm_t* vm, msg_t *msg)
+ * \brief  Send message function.
+ *
+ * \param virtual module who send.
+ * \param msg Message to send to the slave.
+ *
+ * \return send or not
+ */
+unsigned char luos_send(vm_t* vm, msg_t *msg);
+
 
 #endif /* LUOS_H */
--- src/luos.c
+++ src/luos.c
@@ -1,6 +1,19 @@
 #include "luos.h"
 #include "l0.h"
+#include "robus.h"
 #include <string.h>
+
+static vm_t *luos_vm_pointer;
+static volatile msg_t luos_pub_msg;
+static volatile int luos_pub = LUOS_PROTOCOL_NB;
+
+// Luos unic ID => ARM serial number
+typedef struct __attribute__((__packed__)){
+    union {
+        uint32_t uuid[3];
+        uint8_t unmap[3 * sizeof(uint32_t)];                /*!< Uncmaped form. */
+    };
+}luos_uuid_t;
 
 int luos_msg_handler(vm_t* vm, msg_t* input, msg_t* output) {
     if (input->header.cmd == IDENTIFY_CMD) {
@@ -38,4 +51,53 @@ int luos_msg_handler(vm_t* vm, msg_t* input, msg_t* output) {
         return UUID;
     }
     return LUOS_PROTOCOL_NB;
+}
+
+
+void luos_cb(vm_t *vm, msg_t *msg) {
+    // Luos message management
+    int pub_type = luos_msg_handler(vm, msg, &luos_pub_msg);
+    if (pub_type != LUOS_PROTOCOL_NB) {
+        luos_vm_pointer = vm;
+        luos_pub = pub_type;
+        return;
+    }
+    // L0 message management
+    pub_type = l0_msg_handler(vm, msg, &luos_pub_msg);
+    if (pub_type == L0_LED) {
+        return;
+    }
+    if (pub_type != LUOS_PROTOCOL_NB) {
+        luos_vm_pointer = vm;
+        luos_pub = pub_type;
+        return;
+    }
+    if (vm->rx_cb != 0) {
+        vm->rx_cb(vm, msg);
+    }
+}
+
+
+void luos_init(void){
+    robus_init(luos_cb);
+}
+
+
+void luos_loop(void) {
+    if (luos_pub != LUOS_PROTOCOL_NB) {
+      robus_send(luos_vm_pointer, &luos_pub_msg);
+      luos_pub = LUOS_PROTOCOL_NB;
+    }
+}
+
+void luos_modules_clear(void) {
+    robus_modules_clear();
+}
+
+vm_t* luos_module_create(RX_CB mod_cb, unsigned char type, const char *alias) {
+    return robus_module_create(mod_cb, type, alias);
+}
+
+unsigned char luos_send(vm_t* vm, msg_t *msg){
+    return robus_send(vm, msg);
 }
