0654187fc7683d9a9e674ac416feec86141de328
Nicolas Rabault <nicolas.rabault@pollen-robotics.com>
2019-09-17 17:40:15 +0200

    separate message manager

--- src/luos.c
+++ src/luos.c
@@ -3,98 +3,13 @@
 #include "robus.h"
 #include "sys_msg.h"
 #include <string.h>
+#include "message_mngr.h"
 
 static module_t *luos_module_pointer;
 static volatile msg_t luos_pub_msg;
 static volatile int luos_pub = LUOS_PROTOCOL_NB;
 module_t module_table[MAX_VM_NUMBER];
 unsigned char module_number;
-
-// no real time callback management
-volatile int module_msg_available = 0;
-volatile module_t* module_msg_mngr[MSG_BUFFER_SIZE];
-
-/**
- * \struct module_t
- * \brief Module Structure
- *
- * This structure is used to manage modules
- * please refer to the documentation
- */
-typedef struct __attribute__((__packed__)) mngr_t{
-    module_t* module;
-    msg_t* msg;                            /*!< msg ready to be read */
-}mngr_t;
-
-//**************** mngr things*********************
-void mngr_set(module_t* module, msg_t* msg) {
-    if ((module_msg_available+1 > MSG_BUFFER_SIZE) || (module->message_available+1 > MSG_BUFFER_SIZE)) {
-        // This new message doesn't fit into buffer, don't save it
-        return;
-    }
-    module_msg_mngr[module_msg_available++] = module;
-    module->msg_stack[module->message_available++] = msg;
-}
-
-void mngr_get_msg(int module_index,int msg_index, mngr_t* chunk){
-    if ((module_index < 0) | (msg_index < 0)){
-        return;
-    }
-    // get module
-    chunk->module = module_msg_mngr[module_index];
-    __disable_irq();
-    for (int i = module_index; i < module_msg_available; i++) {
-        module_msg_mngr[i] = module_msg_mngr[i+1];
-    }
-    module_msg_available--;
-    __enable_irq();
-
-    // get msg
-    chunk->msg = chunk->module->msg_stack[msg_index];
-    __disable_irq();
-    for (int i = msg_index; i < chunk->module->message_available; i++) {
-        chunk->module->msg_stack[i] = chunk->module->msg_stack[i+1];
-    }
-    chunk->module->message_available--;
-    __enable_irq();
-}
-
-void mngr_get(int module_index, mngr_t* chunk){
-    mngr_get_msg(module_index, 0, chunk);
-}
-
-// find the next message for a module with a callback
-int get_next_cb_id() {
-    for (int i = 0; i < module_msg_available; i++) {
-        if (module_msg_mngr[i]->mod_cb){
-            //there is a callback in this module return this id
-            return i;
-        }
-    }
-    return -1;
-}
-
-// find the next message for a specific module
-int get_next_module_id(module_t* module) {
-    for (int i = 0; i < module_msg_available; i++) {
-        if (module_msg_mngr[i] == module){
-            //there is the module we are looking for
-            return i;
-        }
-    }
-    return -1;
-}
-
-// find the next message from a specific id for a specific module
-int get_next_msg_id(int mngr_id, short msg_from) {
-    // find the next message from the specified id
-    for (int i=0; i<module_msg_mngr[mngr_id]->message_available; i++) {
-        if (module_msg_mngr[mngr_id]->msg_stack[i]->header.source == msg_from) {
-            return i;
-        }
-    }
-    return -1;
-}
 
 //**************** Private functions*********************
 static int luos_msg_handler(module_t* module, msg_t* input, msg_t* output) {
@@ -359,7 +274,7 @@ msg_t* luos_read_from(module_t* module, short id) {
 }
 
 char luos_message_available(void) {
-    return module_msg_available;
+    return mngr_message_available();
 }
 
 void luos_save_alias(module_t* module, char* alias) {
---
+++ src/message_mngr.c
@@ -0,0 +1,88 @@
+/*
+ * message_mngr.c
+ *
+ *  Created on: 17 sept. 2019
+ *      Author: Nicolas Rabault
+ */
+
+#include "message_mngr.h"
+
+// no real time callback management
+volatile int module_msg_available = 0;
+volatile module_t* module_msg_mngr[MSG_BUFFER_SIZE];
+
+//**************** mngr things*********************
+
+char mngr_message_available(void) {
+    return module_msg_available;
+}
+
+void mngr_set(module_t* module, msg_t* msg) {
+    if ((module_msg_available+1 > MSG_BUFFER_SIZE) || (module->message_available+1 > MSG_BUFFER_SIZE)) {
+        // This new message doesn't fit into buffer, don't save it
+        return;
+    }
+    module_msg_mngr[module_msg_available++] = module;
+    module->msg_stack[module->message_available++] = msg;
+}
+
+void mngr_get_msg(int module_index,int msg_index, mngr_t* chunk) {
+    if ((module_index < 0) | (msg_index < 0)){
+        return;
+    }
+    // get module
+    chunk->module = module_msg_mngr[module_index];
+    __disable_irq();
+    for (int i = module_index; i < module_msg_available; i++) {
+        module_msg_mngr[i] = module_msg_mngr[i+1];
+    }
+    module_msg_available--;
+    __enable_irq();
+
+    // get msg
+    chunk->msg = chunk->module->msg_stack[msg_index];
+    __disable_irq();
+    for (int i = msg_index; i < chunk->module->message_available; i++) {
+        chunk->module->msg_stack[i] = chunk->module->msg_stack[i+1];
+    }
+    chunk->module->message_available--;
+    __enable_irq();
+}
+
+void mngr_get(int module_index, mngr_t* chunk) {
+    mngr_get_msg(module_index, 0, chunk);
+}
+
+// find the next message for a module with a callback
+int get_next_cb_id() {
+    for (int i = 0; i < module_msg_available; i++) {
+        if (module_msg_mngr[i]->mod_cb){
+            //there is a callback in this module return this id
+            return i;
+        }
+    }
+    return -1;
+}
+
+// find the next message for a specific module
+int get_next_module_id(module_t* module) {
+    for (int i = 0; i < module_msg_available; i++) {
+        if (module_msg_mngr[i] == module){
+            //there is the module we are looking for
+            return i;
+        }
+    }
+    return -1;
+}
+
+// find the next message from a specific id for a specific module
+int get_next_msg_id(int mngr_id, short msg_from) {
+    // find the next message from the specified id
+    for (int i=0; i<module_msg_mngr[mngr_id]->message_available; i++) {
+        if (module_msg_mngr[mngr_id]->msg_stack[i]->header.source == msg_from) {
+            return i;
+        }
+    }
+    return -1;
+}
+
