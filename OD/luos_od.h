4dbdfd7726ab5fed42ac708de9b7d36273908f82
Nicolas Rabault <nicolas.rabault@pollen-robotics.com>
2019-12-05 12:47:18 +0100

    add object dictionary

---
+++ OD/luos_od.h
@@ -0,0 +1,13 @@
+/*
+ * Luos_od.h
+ *
+ *  Created on: 2 déc. 2019
+ *      Author: nicolasrabault
+ */
+
+#ifndef OD_LUOS_OD_H_
+#define OD_LUOS_OD_H_
+
+#include "od_distance.h"
+
+#endif /* OD_LUOS_OD_H_ */
