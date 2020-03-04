802d3d33eaa6478eac609a9aa03e7a812bc34b79
AntoineVenier <antoine.venier@hotmail.com>
2020-03-04 15:05:35 +0100

    add keyword in library.json and deleted the last part in selected_board_script.py

--- select_board_script.py
+++ select_board_script.py
@@ -7,13 +7,3 @@ for item in env.get("CPPDEFINES", []):
         env.Append(CPPPATH=[realpath(join("hal", item[1]))])
         env.Replace(SRC_FILTER=["+<../src/*>", "-<../hal>", "+<%s>" % join("../hal", item[1])])
         break
-
-# pass flags to a global build environment (for all libraries, etc)
-global_env = DefaultEnvironment()
-global_env.Append(
-    CPPDEFINES=[
-        ("MQTT_MAX_PACKET_SIZE", 512),
-        "ARDUINOJSON_ENABLE_STD_STRING",
-        ("BUFFER_LENGTH", 32)
-    ]
-)
