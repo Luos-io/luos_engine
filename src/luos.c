c177cda3aaeea4dac2df79d72af6411020f52781
AntoineVenier <antoine.venier@hotmail.com>
2020-04-30 11:48:02 +0200

    change luos version from unknown to v0.6 in luos.c file

--- src/luos.c
+++ src/luos.c
@@ -45,9 +45,9 @@ static int luos_msg_handler(module_t* module, msg_t* input, msg_t* output) {
         output->header.cmd = LUOS_REVISION;
         output->header.target_mode = ID;
 #ifndef LUOS_REV
-#define LUOS_REV "unknown"
+#define LUOS_REV "V0.6"
 #endif
-        memcpy(output->data, LUOS_REV, sizeof("unknown"));
+        memcpy(output->data, LUOS_REV, sizeof("V0.6"));
         output->header.size = strlen((char*)output->data);
         output->header.target = input->header.source;
         luos_pub = LUOS_REVISION;
