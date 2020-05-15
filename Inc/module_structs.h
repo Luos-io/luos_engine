7dfd0e50a4d0c6ee2066daeda840639550882bde
Nicolas Rabault <nicolas.rabault@luos-robotics.com>
2020-05-15 14:11:07 +0200

    remove motor structs

--- Inc/module_structs.h
+++ Inc/module_structs.h
@@ -56,7 +56,7 @@ typedef struct __attribute__((__packed__))
     };
 } luos_uuid_t;
 
-// Handy struct
+// Handy struct TODO : remove it
 typedef struct __attribute__((__packed__))
 {
     union {
@@ -121,32 +121,6 @@ typedef struct
     };
 } servo_parameters_t;
 
-// Motor
-typedef struct __attribute__((__packed__))
-{
-    union {
-        struct __attribute__((__packed__))
-        {
-            // target modes
-            uint16_t mode_compliant : 1;
-            uint16_t mode_ratio : 1;
-            uint16_t mode_torque : 1;
-            uint16_t mode_angular_speed : 1;
-            uint16_t mode_angular_position : 1;
-            uint16_t mode_linear_speed : 1;
-            uint16_t mode_linear_position : 1;
-
-            // report modes
-            uint16_t angular_position : 1;
-            uint16_t angular_speed : 1;
-            uint16_t linear_position : 1;
-            uint16_t linear_speed : 1;
-            uint16_t current : 1;
-        };
-        uint8_t unmap[2];
-    };
-} motor_mode_t;
-
 // Control modes
 typedef enum
 {
@@ -167,32 +141,5 @@ typedef struct __attribute__((__packed__))
         uint8_t unmap;
     };
 } control_mode_t;
-
-typedef struct __attribute__((__packed__))
-{
-
-    // targets
-    motor_mode_t mode;
-    angular_position_t target_angular_position;
-    angular_speed_t target_angular_speed;
-    ratio_t target_ratio;
-
-    // limits
-    angular_position_t limit_angular_position[2];
-    ratio_t limit_ratio;
-    current_t limit_current;
-
-    // measures
-    angular_position_t angular_position;
-    angular_speed_t angular_speed;
-    linear_position_t linear_position;
-    linear_speed_t linear_speed;
-    current_t current;
-
-    //configs
-    float motor_reduction;
-    float resolution;
-    linear_position_t wheel_diameter;
-} motor_config_t;
 
 #endif /*__ __L0_H */
