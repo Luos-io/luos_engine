d3d6a0ae14e141326ff494fbf89d4fa861b2e6dd
Nicolas Rabault <nicolas.rabault@luos.io>
2020-09-14 15:07:00 +0200

    remove not used anymore folders

--- hal/orbita/node_config.h
+++
@@ -1,8 +0,0 @@
-#ifndef __NODE_CONFIG_H
-#define __NODE_CONFIG_H
-// L0 pins configurations
-#define ROBUS_POWER_SENSOR_Pin GPIO_PIN_1
-#define ROBUS_POWER_SENSOR_GPIO_Port GPIOB
-#define LED_Pin GPIO_PIN_15
-#define LED_GPIO_Port GPIOC
-#endif /* __NODE_CONFIG_H */
