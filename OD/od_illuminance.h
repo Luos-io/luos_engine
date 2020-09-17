5a6ae7b207b3bc153f008cbf137d5232e537d379
Salem-Luos <thomas.salembier@luos.io>
2020-09-17 18:43:56 +0200

    formating for OD files

--- OD/od_illuminance.h
+++ OD/od_illuminance.h
@@ -1,38 +1,47 @@
-/*
- * od_illuminance.h
- *
- *  Created on: 7 déc. 2019
- *      Author: nicolasrabault
- */
-
+/******************************************************************************
+ * @file OD_illuminance
+ * @brief object dictionnary illuminance
+ * @author Luos
+ * @version 0.0.0
+ ******************************************************************************/
 #ifndef OD_OD_ILLUMINANCE_H_
 #define OD_OD_ILLUMINANCE_H_
 
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
 typedef float illuminance_t;
 
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Function
+ ******************************************************************************/
 // illuminance are stored in lux (lx or lm/m2)
 //******** Conversions ***********
 
 // lx
-static inline float illuminance_to_lx(illuminance_t self)
+static inline float IlluminanceOD_IlluminanceTo_Lux(illuminance_t self)
 {
     return self;
 }
 
-static inline illuminance_t illuminance_from_lx(float lx)
+static inline illuminance_t IlluminanceOD_IlluminanceFrom_Lux(float lx)
 {
     return lx;
 }
 
 //******** Messages management ***********
-static inline void illuminance_to_msg(const illuminance_t *const self, msg_t *const msg)
+static inline void IlluminanceOD_IlluminanceToMsg(const illuminance_t *const self, msg_t *const msg)
 {
     msg->header.cmd = ILLUMINANCE;
     memcpy(msg->data, self, sizeof(illuminance_t));
     msg->header.size = sizeof(illuminance_t);
 }
 
-static inline void illuminance_from_msg(illuminance_t *const self, const msg_t *const msg)
+static inline void IlluminanceOD_IlluminanceFromMsg(illuminance_t *const self, const msg_t *const msg)
 {
     memcpy(self, msg->data, msg->header.size);
 }
@@ -53,14 +62,14 @@ typedef struct __attribute__((__packed__))
 
 // color are stored in RGB
 //******** Messages management ***********
-static inline void color_to_msg(const color_t *const self, msg_t *const msg)
+static inline void IlluminanceOD_ColorToMsg(const color_t *const self, msg_t *const msg)
 {
     msg->header.cmd = COLOR;
     memcpy(msg->data, self, sizeof(color_t));
     msg->header.size = sizeof(color_t);
 }
 
-static inline void color_from_msg(color_t *const self, const msg_t *const msg)
+static inline void IlluminanceOD_ColorFromMsg(color_t *const self, const msg_t *const msg)
 {
     memcpy(self, msg->data, msg->header.size);
 }
