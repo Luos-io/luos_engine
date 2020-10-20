1b33977164d6a57b572ac348e00cf96188bf52b4
Nicolas Rabault <nicolas.rabault@luos.io>
2020-10-20 15:01:45 +0200

    add detection files to Luos.

---
+++ inc/detection.h
@@ -0,0 +1,10 @@
+/******************************************************************************
+ * @file detection
+ * @brief manage detection related functions.
+ * @author Luos
+ * @version 0.0.0
+ ******************************************************************************/
+#ifndef DETECTION
+#define DETECTION
+
+#endif /* DETECTION */
