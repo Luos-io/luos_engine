pierre-rouanet/Luos

Change Timeout in Luos

Robus/inc/robus.h
@@ -27,5 +27,6 @@ void Robus_ContainersClear(void);
 error_return_t Robus_SendMsg(ll_container_t *ll_container, msg_t *msg);
 uint16_t Robus_TopologyDetection(ll_container_t *ll_container);
 node_t *Robus_GetNode(void);
+void Robus_DelayUs(uint16_t delay);
 
 #endif /* _ROBUS_H_ */

Luos-io/luos_engine

Robus/inc/robus.h
@@ -27,5 +27,6 @@ void Robus_ContainersClear(void);
 error_return_t Robus_SendMsg(ll_container_t *ll_container, msg_t *msg);
 uint16_t Robus_TopologyDetection(ll_container_t *ll_container);
 node_t *Robus_GetNode(void);
+void Robus_DelayUs(uint32_t delay);
 
 #endif /* _ROBUS_H_ */