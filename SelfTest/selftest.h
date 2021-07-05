3a52bda3fc209f5f723b79aecaa22ed446932408
nicolas cros <nicolas.cros@luos.io>
2021-07-05 15:33:15 +0200

    add a selftest feature to the luos library

---
+++ SelfTest/selftest.h
@@ -0,0 +1,14 @@
+/******************************************************************************
+ * @file luos selftest
+ * @brief function to test luos library
+ * @author Luos
+ * @version 0.0.0
+ ******************************************************************************/
+#ifndef SELFTEST_H
+#define SELFTEST_H
+
+#include "luos.h"
+
+void selftest_run(void);
+
+#endif /* LUOS_H */
