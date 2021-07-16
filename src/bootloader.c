ee6ad095d5c0d0f8582e1f0e7e4369ebf16621d3
nicolas cros <nicolas.cros@luos.io>
2021-07-16 18:18:38 +0200

    compile specific bootloader code only in bootloader application

--- src/bootloader.c
+++ src/bootloader.c
@@ -17,32 +17,36 @@
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
-#define MAX_FRAME_SIZE 127
+#ifdef BOOTLOADER_CONFIG
+#define MAX_FRAME_SIZE (MAX_DATA_MSG_SIZE - 1)
+#define BUFFER_SIZE    0x800 // 2kB buffer to store received data
+#endif
 
 /*******************************************************************************
  * Variables
  ******************************************************************************/
-static bootloader_state_t bootloader_state = BOOTLOADER_START_STATE;
 static bootloader_cmd_t bootloader_cmd;
 
+#ifdef BOOTLOADER_CONFIG
+static bootloader_state_t bootloader_state = BOOTLOADER_START_STATE;
 // variables use to save binary data in flash
 static uint8_t bootloader_data[MAX_FRAME_SIZE];
 static uint16_t bootloader_data_size = 0;
 
-uint32_t page_addr = APP_ADDRESS;
-uint8_t page_buff[(uint16_t)PAGE_SIZE];
+uint32_t flash_addr = APP_ADDRESS;
+uint8_t data_buff[(uint16_t)BUFFER_SIZE];
 uint16_t data_index     = 0;
-uint16_t residual_space = (uint16_t)PAGE_SIZE;
-uint16_t page_id        = APP_FLASH_PAGE;
+uint16_t residual_space = (uint16_t)BUFFER_SIZE;
 uint32_t nb_bytes       = 0;
+#endif
 
 /*******************************************************************************
  * Function
  ******************************************************************************/
+#ifdef BOOTLOADER_CONFIG
 static uint8_t LuosBootloader_GetMode(void);
 static void LuosBootloader_DeInit(void);
 static void LuosBootloader_JumpToApp(void);
-static void LuosBootloader_SaveNodeID(void);
 static void LuosBootloader_SetNodeID(void);
 static inline uint8_t LuosBootloader_IsEnoughSpace(uint32_t);
 static inline void LuosBootloader_EraseMemory(void);
@@ -52,7 +56,11 @@ static void LuosBootloader_SendResponse(bootloader_cmd_t);
 static void LuosBootloader_SendCrc(bootloader_cmd_t, uint8_t, uint8_t);
 static void LuosBootloader_SetState(bootloader_state_t);
 static void LuosBootloader_Task(void);
+#else
+static void LuosBootloader_SaveNodeID(void);
+#endif
 
+#ifdef BOOTLOADER_CONFIG
 /******************************************************************************
  * @brief read the boot mode in flash memory
  * @param None
@@ -62,7 +70,9 @@ uint8_t LuosBootloader_GetMode(void)
 {
     return LuosHAL_GetMode();
 }
+#endif
 
+#ifdef BOOTLOADER_CONFIG
 /******************************************************************************
  * @brief DeInit peripherals in bootloader
  * @param None
@@ -72,7 +82,9 @@ void LuosBootloader_DeInit(void)
 {
     LuosHAL_DeInit();
 }
+#endif
 
+#ifdef BOOTLOADER_CONFIG
 /******************************************************************************
  * @brief launch application from the bootloader
  * @param None
@@ -82,6 +94,7 @@ void LuosBootloader_JumpToApp(void)
 {
     LuosHAL_JumpToApp(APP_ADDRESS);
 }
+#endif
 
 /******************************************************************************
  * @brief Save node id in flash
@@ -95,6 +108,7 @@ void LuosBootloader_SaveNodeID(void)
     LuosHAL_SaveNodeID(node_id);
 }
 
+#ifdef BOOTLOADER_CONFIG
 /******************************************************************************
  * @brief Set node id with data saved in flash
  * @param None
@@ -106,7 +120,9 @@ void LuosBootloader_SetNodeID(void)
 
     Robus_SetNodeID(node_id);
 }
+#endif
 
+#ifdef BOOTLOADER_CONFIG
 /******************************************************************************
  * @brief Set node id with data saved in flash
  * @param None
@@ -124,6 +140,9 @@ uint8_t LuosBootloader_IsEnoughSpace(uint32_t binary_size)
         return 0x00;
     }
 }
+#endif
+
+#ifdef BOOTLOADER_CONFIG
 /******************************************************************************
  * @brief process binary data received from the gate
  * @param None 
@@ -135,6 +154,7 @@ void LuosBootloader_EraseMemory(void)
 }
 #endif
 
+#ifdef BOOTLOADER_CONFIG
 /******************************************************************************
  * @brief process binary data received from the gate
  * @param None 
@@ -146,7 +166,7 @@ void LuosBootloader_ProcessData(void)
     {
         // there is enough space in the current page
         // fill the current page with data
-        memcpy(&page_buff[data_index], bootloader_data, bootloader_data_size);
+        memcpy(&data_buff[data_index], bootloader_data, bootloader_data_size);
 
         // update data_index and residual_space
         data_index += bootloader_data_size;
@@ -155,26 +175,27 @@ void LuosBootloader_ProcessData(void)
     else
     {
         // complete the current page buffer
-        memcpy(&page_buff[data_index], bootloader_data, residual_space);
+        memcpy(&data_buff[data_index], bootloader_data, residual_space);
 
         // save the completed page in flash memory
-        LuosHAL_ProgramFlash(page_addr, page_id, (uint16_t)PAGE_SIZE, page_buff);
+        LuosHAL_ProgramFlash(flash_addr, (uint16_t)BUFFER_SIZE, data_buff);
 
         // prepare next page buffer
-        page_addr += PAGE_SIZE;
-        page_id += 1;
+        flash_addr += BUFFER_SIZE;
         data_index = 0;
-        memset(page_buff, 0xFF, (uint16_t)PAGE_SIZE);
+        memset(data_buff, 0xFF, (uint16_t)BUFFER_SIZE);
 
         // copy the remaining data in the new page buffer
-        memcpy(&page_buff[data_index], &bootloader_data[residual_space], bootloader_data_size - residual_space);
+        memcpy(&data_buff[data_index], &bootloader_data[residual_space], bootloader_data_size - residual_space);
 
         // update data_index and residual_space
         data_index     = bootloader_data_size - residual_space;
-        residual_space = (uint16_t)PAGE_SIZE - data_index;
+        residual_space = (uint16_t)BUFFER_SIZE - data_index;
     }
 }
+#endif
 
+#ifdef BOOTLOADER_CONFIG
 /******************************************************************************
  * @brief Save the current page when BIN_END command is received 
  * @param None 
@@ -182,9 +203,11 @@ void LuosBootloader_ProcessData(void)
  ******************************************************************************/
 void LuosBootloader_SaveLastData(void)
 {
-    LuosHAL_ProgramFlash(page_addr, page_id, (uint16_t)PAGE_SIZE, page_buff);
+    LuosHAL_ProgramFlash(flash_addr, (uint16_t)BUFFER_SIZE, data_buff);
 }
+#endif
 
+#ifdef BOOTLOADER_CONFIG
 /******************************************************************************
  * @brief compute crc 8 for each data
  * @param data pointer, data len 
@@ -212,7 +235,9 @@ void crc8(const uint8_t *data, uint8_t *crc, uint16_t polynome)
 
     *crc = (uint8_t)(dbyte >> 8);
 }
+#endif
 
+#ifdef BOOTLOADER_CONFIG
 /******************************************************************************
  * @brief compute crc for the whole binary
  * @param data pointer, data len 
@@ -250,7 +275,9 @@ uint8_t compute_crc(void)
 
     return crc;
 }
+#endif
 
+#ifdef BOOTLOADER_CONFIG
 /******************************************************************************
  * @brief Send response to the gate
  * @param None
@@ -268,7 +295,9 @@ void LuosBootloader_SendCrc(bootloader_cmd_t response, uint8_t data, uint8_t siz
 
     Luos_SendMsg(0, &ready_msg);
 }
+#endif
 
+#ifdef BOOTLOADER_CONFIG
 /******************************************************************************
  * @brief Send response to the gate
  * @param None
@@ -284,7 +313,9 @@ void LuosBootloader_SendResponse(bootloader_cmd_t response)
     ready_msg.data[0]            = response;
     Luos_SendMsg(0, &ready_msg);
 }
+#endif
 
+#ifdef BOOTLOADER_CONFIG
 /******************************************************************************
  * @brief set state of the bootloader
  * @param state 
@@ -294,7 +325,9 @@ void LuosBootloader_SetState(bootloader_state_t state)
 {
     bootloader_state = state;
 }
+#endif
 
+#ifdef BOOTLOADER_CONFIG
 /******************************************************************************
  * @brief Bootloader machine state
  * @param None
@@ -349,6 +382,7 @@ void LuosBootloader_Task(void)
                 // go to HEADER state
                 LuosBootloader_SetState(BOOTLOADER_BIN_CHUNK_STATE);
             }
+
             break;
 
         case BOOTLOADER_BIN_CHUNK_STATE:
@@ -392,6 +426,8 @@ void LuosBootloader_Task(void)
         case BOOTLOADER_STOP_STATE:
             // save boot_mode in flash
             LuosHAL_SetMode(APPLICATION_MODE);
+            // wait for the command to be send to all nodes
+            LuosHAL_Delay(1000);
             // reboot the node
             LuosHAL_Reboot();
             break;
@@ -400,6 +436,37 @@ void LuosBootloader_Task(void)
             break;
     }
 }
+#endif
+
+#ifdef BOOTLOADER_CONFIG
+/******************************************************************************
+ * @brief bootloader app
+ * @param None
+ * @return None
+ ******************************************************************************/
+void LuosBootloader_Run(void)
+{
+    switch (LuosBootloader_GetMode())
+    {
+        case APPLICATION_MODE:
+            // boot the application programmed in dedicated flash partition
+            LuosBootloader_DeInit();
+            LuosBootloader_JumpToApp();
+            break;
+
+        case BOOTLOADER_MODE:
+        default:
+            Luos_Init();
+            Boot_Init(); // pour le debug
+            while (1)
+            {
+                Luos_Loop();
+                LuosBootloader_Task();
+            }
+            break;
+    }
+}
+#endif
 
 /******************************************************************************
  * @brief Message handler called from luos library
@@ -412,6 +479,7 @@ void LuosBootloader_MsgHandler(msg_t *input)
 
     switch (bootloader_cmd)
     {
+#ifndef BOOTLOADER_CONFIG
         case BOOTLOADER_START:
             // We're in the app,
             // set bootloader mode, save node ID and reboot
@@ -419,7 +487,7 @@ void LuosBootloader_MsgHandler(msg_t *input)
             LuosBootloader_SaveNodeID();
             LuosHAL_Reboot();
             break;
-
+#else
         case BOOTLOADER_STOP:
         case BOOTLOADER_READY:
         case BOOTLOADER_BIN_CHUNK:
@@ -430,38 +498,7 @@ void LuosBootloader_MsgHandler(msg_t *input)
             bootloader_data_size = input->header.size - sizeof(char);
             memcpy(bootloader_data, &(input->data[1]), bootloader_data_size);
             break;
-
-        default:
-            break;
-    }
-}
-
-/******************************************************************************
- * @brief bootloader app
- * @param None
- * @return None
- ******************************************************************************/
-void LuosBootloader_Run(void)
-{
-    switch (LuosBootloader_GetMode())
-    {
-        case BOOTLOADER_MODE:
-            Luos_Init();
-            Boot_Init(); // pour le debug
-            while (1)
-            {
-                Luos_Loop();
-                Boot_Loop(); // pour le debug
-                LuosBootloader_Task();
-            }
-            break;
-
-        case APPLICATION_MODE:
-            // boot the application programmed in dedicated flash partition
-            LuosBootloader_DeInit();
-            LuosBootloader_JumpToApp();
-            break;
-
+#endif
         default:
             break;
     }
