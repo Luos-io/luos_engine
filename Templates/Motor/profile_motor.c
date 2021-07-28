5cd79465d4a31a856bf4406319d21d1d91941989
nicolas cros <nicolas.cros@luos.io>
2021-07-28 18:14:34 +0200

    Motor profile uses core profile API

--- Templates/Motor/profile_motor.c
+++ Templates/Motor/profile_motor.c
@@ -1,63 +1,17 @@
-#include "template_motor.h"
 #include "profile_motor.h"
 #include "math.h"
+
 /******************************************************************************
- * @brief function converting Luos messages innto data and reverse.
+ * @brief function converting Luos messages into data and reverse.
  * @param service the target service
  * @param msg the received message
- * @param motor the data struct to update
  * @return None
  ******************************************************************************/
-static void MotorConfig_Handler(service_t *service, msg_t *msg, profile_motor_t *motor_profile)
+void Luos_MotorHandler(service_t *service, msg_t *msg)
 {
-    if (msg->header.cmd == PARAMETERS)
-    {
-        // check the message size
-        // fill the message infos
-        memcpy((void *)&motor_profile->mode, msg->data, sizeof(motor_mode_t));
-        return;
-    }
-}
-/******************************************************************************
- * @brief Msg Handler call backed by Luos when a msg receive for this service
- * @param Service destination
- * @param Msg receive
- * @return None
- ******************************************************************************/
-static void TemplateMotor_MsgHandler(service_t *service, msg_t *msg)
-{
-    template_motor_t *motor_template = (template_motor_t *)service->template_context;
-    ProfileMotor_Handler(service, msg, &motor_template->profile);
-    MotorConfig_Handler(service, msg, &motor_template->profile);
-    if (motor_template->self != 0)
-    {
-        motor_template->self(service, msg);
-    }
-}
-/******************************************************************************
- * @brief Service creation following the template
- * @param service_cb is an optional user callback called on every massage for this service
- * @param motor_struct template object pointer
- * @param alias for the service string (15 caracters max).
- * @param revision FW for the service (tab[MajorVersion,MinorVersion,Patch])
- * @return None
- ******************************************************************************/
-service_t *TemplateMotor_CreateService(SERVICE_CB service_cb, template_motor_t *motor_template, const char *alias, revision_t revision)
-{
-    motor_template->self      = service_cb;
-    service_t *service        = Luos_CreateService(TemplateMotor_MsgHandler, MOTOR_TYPE, alias, revision);
-    service->template_context = (void *)motor_template;
-    return service;
-}
-/******************************************************************************
- * @brief function converting Luos messages innto data and reverse.
- * @param service the target service
- * @param msg the received message
- * @param motor_struct the data struct to update
- * @return None
- ******************************************************************************/
-void ProfileMotor_Handler(service_t *service, msg_t *msg, profile_motor_t *profile_motor)
-{
+    profile_core_t *profile        = Luos_GetProfileFromService(service);
+    profile_motor_t *profile_motor = (profile_motor_t *)profile->profile_data;
+
     switch (msg->header.cmd)
     {
         case GET_CMD:
@@ -67,12 +21,12 @@ void ProfileMotor_Handler(service_t *service, msg_t *msg, profile_motor_t *profi
             pub_msg.header.target      = msg->header.source;
             if (profile_motor->mode.current)
             {
-                ElectricOD_CurrentToMsg((current_t *)&profile_motor->current, &pub_msg);
+                ElectricOD_CurrentToMsg(&profile_motor->current, &pub_msg);
                 Luos_SendMsg(service, &pub_msg);
             }
             if (profile_motor->mode.temperature)
             {
-                TemperatureOD_TemperatureToMsg((current_t *)&profile_motor->temperature, &pub_msg);
+                TemperatureOD_TemperatureToMsg(&profile_motor->temperature, &pub_msg);
                 Luos_SendMsg(service, &pub_msg);
             }
         }
@@ -110,5 +64,33 @@ void ProfileMotor_Handler(service_t *service, msg_t *msg, profile_motor_t *profi
             profile_motor->limit_temperature = fabs(profile_motor->limit_temperature);
         }
         break;
+        case PARAMETERS:
+        {
+            // check the message size
+            // fill the message infos
+            memcpy((void *)&profile_motor->mode, msg->data, sizeof(motor_mode_t));
+        }
+        break;
     }
+}
+
+/******************************************************************************
+ * @brief Link motor profile to the general profile handler
+ * @param profile handler, 
+ * @param profile_motor handler, 
+ * @param callback used by the profile
+ * @return None
+ ******************************************************************************/
+void Luos_LinkMotorProfile(profile_core_t *profile, profile_motor_t *profile_motor, SERVICE_CB callback)
+{
+    // set general profile handler type
+    profile->type = MOTOR_TYPE;
+
+    // link general profile handler to the profile data structure
+    profile->profile_data = (HANDLER *)profile_motor;
+
+    // set profile handler / callback functions
+    profile->profile_ops.Init     = 0;
+    profile->profile_ops.Handler  = Luos_MotorHandler;
+    profile->profile_ops.Callback = callback;
 }
--- Templates/Motor/profile_motor.h
+++ Templates/Motor/profile_motor.h
@@ -5,15 +5,54 @@
  * @author Luos
  * @version 0.0.0
  ******************************************************************************/
-#ifndef TEMPLATE_CLASS_MOTOR_H_
-#define TEMPLATE_CLASS_MOTOR_H_
+#ifndef PROFILE_MOTOR_H_
+#define PROFILE_MOTOR_H_
 
-#include "struct_motor.h"
+#include "profile_core.h"
+
+#define MINI 0
+#define MAXI 1
 
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
+/*
+ * motor mode structure
+ */
+typedef struct __attribute__((__packed__))
+{
+    union
+    {
+        struct __attribute__((__packed__))
+        {
+            // drive modes
+            uint8_t mode_compliant : 1;
 
+            // measures modes
+            uint8_t temperature : 1;
+            uint8_t current : 1;
+        };
+        uint8_t unmap[1];
+    };
+} motor_mode_t;
+/*
+ * motor data structure
+ */
+typedef struct
+{
+    // drive
+    motor_mode_t mode;
+    ratio_t power;
+
+    // limits
+    ratio_t limit_ratio;
+    current_t limit_current;
+    temperature_t limit_temperature;
+
+    // measures
+    temperature_t temperature;
+    current_t current;
+} profile_motor_t;
 /*******************************************************************************
  * Variables
  ******************************************************************************/
@@ -21,7 +60,7 @@
 /*******************************************************************************
  * Function
  ******************************************************************************/
+void Luos_MotorHandler(service_t *service, msg_t *msg);
+void Luos_LinkMotorProfile(profile_core_t *profile, profile_motor_t *profile_motor, SERVICE_CB callback);
 
-void ProfileMotor_Handler(service_t *service, msg_t *msg, profile_motor_t *motor_profile);
-
-#endif /* TEMPLATE_CLASS_MOTOR_H_ */
+#endif /* PROFILE_MOTOR_H_ */
