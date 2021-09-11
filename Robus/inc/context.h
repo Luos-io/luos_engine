e2309653ed9ab9f7fab8bc5ccc680e7a2a3ea042
Nicolas Rabault <nicolas.rabault@luos.io>
2021-09-11 16:04:08 +0200

    Improve comments

--- Robus/inc/context.h
+++ Robus/inc/context.h
@@ -22,11 +22,11 @@ typedef struct
     node_t node;    /*!< Node informations. */
     RxCom_t rx;     /*!< Receiver informations. */
     TxCom_t tx;     /*!< Transmitter informations. */
-    PortMng_t port; /*!< port informations. */
+    PortMng_t port; /*!< Port informations. */
 
-    //Virtual service management
-    ll_service_t ll_service_table[MAX_SERVICE_NUMBER]; /*!< Virtual Service table. */
-    uint16_t ll_service_number;                        /*!< Virtual Service number. */
+    // Low level service management
+    ll_service_t ll_service_table[MAX_SERVICE_NUMBER]; /*!< Low level Service table. */
+    uint16_t ll_service_number;                        /*!< Low level Service number. */
 
 } context_t;
 
