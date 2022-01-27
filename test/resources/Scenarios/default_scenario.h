b55be0e1afb7da8d43b5fd6119c6431c7bf35c80
JeromeGalan <jerome.galan@luos.io>
2022-01-27 15:07:08 +0100

    Add a default scenario for unit tests

---
+++ test/resources/Scenarios/default_scenario.h
@@ -0,0 +1,52 @@
+#ifndef DEFAULT_SCENARIO_H
+#define DEFAULT_SCENARIO_H
+
+#include "unit_test.h"
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+#define DUMMY_SERVICE_NUMBER 3
+#define STREAM_BUFFER_SIZE   1024
+
+typedef struct
+{
+    service_t *app;
+    msg_t *tx_msg;
+    msg_t *last_rx_msg;
+    streaming_channel_t streamChannel;
+    //next here
+    /*
+        * streaming_channel_t à remplir
+        * buffer cmd à remplir
+        * task à remplir
+    */
+
+} dummy_app_t;
+
+typedef struct
+{
+    dummy_app_t App_1;
+    dummy_app_t App_2;
+    dummy_app_t App_3;
+} default_scenario_t;
+
+typedef enum
+{
+    DEFAULT_CMD = LUOS_LAST_STD_CMD // a dummy command for apps
+} default_scenario_cmd_t;
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+default_scenario_t default_sc;
+
+/*******************************************************************************
+ * Function
+ ******************************************************************************/
+void Init_Context(void);
+void App_1_Loop(void);
+void App_2_Loop(void);
+void App_3_Loop(void);
+
+#endif //DEFAULT_SCENARIO_H
