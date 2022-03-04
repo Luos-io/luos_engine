cf849652c07a88ae135f0bdca19aed03277adfcb
Nicolas Rabault <nicolas.rabault@luos.io>
2022-03-04 16:48:52 +0100

    Move Luos code into Luos_engine folder

--- source_filter_script.py
+++ source_filter_script.py
@@ -7,12 +7,12 @@ for item in env.get("CPPDEFINES", []):
         print("Selected HAL for Luos and Robus is : %s" % item[1])
         env.Append(CPPPATH=[realpath("Network/Robus/HAL/" + item[1])])
         env.Replace(SRC_FILTER=["+<*.c>",
-                                "+<../Network/Robus/src/*.c>",
-                                "+<../Network/Robus/HAL/%s/*.c>" % item[1],
-                                "+<../Profiles/Core/*.c>",
-                                "+<../Profiles/State/*.c>",
-                                "+<../Profiles/Motor/*.c>",
-                                "+<../Profiles/Servo_motor/*.c>",
-                                "+<../Profiles/Voltage/*.c>",
-                                "+<../Bootloader/*.c>"])
+                                "+<../../../Network/Robus/src/*.c>",
+                                "+<../../../Network/Robus/HAL/%s/*.c>" % item[1],
+                                "+<../../Profiles/Core/*.c>",
+                                "+<../../Profiles/State/*.c>",
+                                "+<../../Profiles/Motor/*.c>",
+                                "+<../../Profiles/Servo_motor/*.c>",
+                                "+<../../Profiles/Voltage/*.c>",
+                                "+<../../Bootloader/*.c>"])
         break
