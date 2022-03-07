669636b5a1f5f1cb7bfa3bb7bc4f719a3e7744f1
Nicolas Rabault <nicolas.rabault@luos.io>
2022-03-07 16:57:29 +0100

    Create a luos.h file including luos_engine.h file for retrocompatibility

---
+++ Luos_engine/Core/inc/luos.h
@@ -0,0 +1,6 @@
+/******************************************************************************
+ * @file luos
+ * @brief Retrocompatibility file to include luos_engine.h instead of luos.h
+ * @author Luos
+ ******************************************************************************/
+#include "luos_engine.h"
