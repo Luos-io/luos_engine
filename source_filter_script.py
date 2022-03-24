253017977a892bacbba1f53ee4851508a6dba9ab
Nicolas Rabault <nicolas.rabault@luos.io>
2022-03-24 13:28:50 +0100

    Avoid multiple source_filter_script execution

--- source_filter_script.py
+++ source_filter_script.py
@@ -4,21 +4,28 @@ from os import system, listdir, path
 from os.path import join, realpath
 Import('env')
 
-# install pyluos
-try:
-    import pyluos
-    subprocess.check_call([sys.executable, "-m", "pip",
-                          "install", "pyluos", "--upgrade", "--quiet"])
-except ImportError:  # module doesn't exist, deal with it.
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "pyluos"])
-    pass
-print('\n\033[4mLuos engine build configuration:\033[0m\n')
-try:
-    from pyluos import version
-    print("\t*\033[0;32m Pyluos revision " +
-          str(version.version) + " ready.\033[0m")
-except ImportError:  # module doesn't exist, deal with it.
-    print("\t*\033[0;32m Pyluos install failed. Platformio will be unable to use bootloader flash feature.\033[0m")
+# Check if this script have been already executed during this compilation
+visited_key = "__LUOS_CORE_SCRIPT_CALLED"
+global_env = DefaultEnvironment()
+
+if not visited_key in global_env:
+    # install pyluos
+    try:
+        import pyluos
+        subprocess.check_call([sys.executable, "-m", "pip",
+                               "install", "pyluos", "--upgrade", "--quiet"])
+    except ImportError:  # module doesn't exist, deal with it.
+        subprocess.check_call(
+            [sys.executable, "-m", "pip", "install", "pyluos"])
+        pass
+    print('\n\033[4mLuos engine build configuration:\033[0m\n')
+    try:
+        from pyluos import version
+        print("\t*\033[0;32m Pyluos revision " +
+              str(version.version) + " ready.\033[0m")
+    except ImportError:  # module doesn't exist, deal with it.
+        print(
+            "\t*\033[0;32m Pyluos install failed. Platformio will be unable to use bootloader flash feature.\033[0m")
 
 sources = ["+<*.c>",
            "+<../../../Network/Robus/src/*.c>",
@@ -35,10 +42,12 @@ for item in env.get("CPPDEFINES", []):
     if isinstance(item, tuple) and item[0] == "LUOSHAL":
         find_HAL = True
         if (path.exists("Network/Robus/HAL/" + item[1]) and path.exists("Engine/HAL/" + item[1])):
-            print(
-                "\t*\033[0;32m %s HAL selected for Luos and Robus.\033[0m\n" % item[1])
+            if not visited_key in global_env:
+                print(
+                    "\t*\033[0;32m %s HAL selected for Luos and Robus.\033[0m\n" % item[1])
         else:
-            print("\t*\033[1;31m %s HAL not found\033[0m\n" % item[1])
+            if not visited_key in global_env:
+                print("\t*\033[1;31m %s HAL not found\033[0m\n" % item[1])
         env.Append(CPPPATH=[realpath("Network/Robus/HAL/" + item[1])])
         env.Append(CPPPATH=[realpath("Engine/HAL/" + item[1])])
         env.Replace(SRC_FILTER=sources)
@@ -47,8 +56,9 @@ for item in env.get("CPPDEFINES", []):
         env.Append(SRC_FILTER=["+<../../HAL/%s/*.c>" % item[1]])
         break
 
-if (find_HAL == False):
-    print("\033[1;31mNo HAL selected. Please add a \033[0;30;47m-DLUOSHAL\033[0m\033[1;31m compilation flag\033[0m\n")
+if not visited_key in global_env:
+    if (find_HAL == False):
+        print("\033[1;31mNo HAL selected. Please add a \033[0;30;47m-DLUOSHAL\033[0m\033[1;31m compilation flag\033[0m\n")
 
 # native unit testing
 if (env.get("PIOPLATFORM") == "native"):
@@ -65,9 +75,12 @@ if (env.get("PIOPLATFORM") == "native"):
                 os.system("lcov -d .pio/build/native/ -c -o lcov.info")
                 os.system(
                     "lcov --remove lcov.info '*/tool-unity/*' '*/test/*' -o filtered_lcov.info")
-                os.system("genhtml -o cov/ --demangle-cpp filtered_lcov.info")
+                os.system(
+                    "genhtml -o cov/ --demangle-cpp filtered_lcov.info")
 
             # Generate code coverage when testing workflow is ended
             env.AddPostAction(".pio/build/native/program",
                               generateCoverageInfo)
             break
+
+global_env[visited_key] = True
