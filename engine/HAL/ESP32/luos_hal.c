9f752abeeddc436a090f76a9b98b34a73baa5db1
Salem_Luos <thomas.salembier@luos.io>
2022-04-22 16:58:41 +0200

    create LuosHal and RobusHal

---
+++ engine/HAL/ESP32/luos_hal.c
@@ -0,0 +1,185 @@
+/******************************************************************************
+ * @file luosHAL
+ * @brief Luos Hardware Abstration Layer. Describe Low layer fonction
+ * @MCU Family ESP32
+ * @author Luos
+ * @version 0.0.0
+ ******************************************************************************/
+#include "luos_hal.h"
+#include "robus_hal.h"
+
+#include <stdbool.h>
+#include <string.h>
+
+// MCU dependencies this HAL is for family ESP32 you can find
+// the HAL ESP-IDF on Expressiv web site
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "sdkconfig.h"
+
+#include "esp_timer.h"
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+// timestamp variable
+static ll_timestamp_t ll_timestamp;
+
+/*******************************************************************************
+ * esp
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Function
+ ******************************************************************************/
+static void LuosHAL_SystickInit(void);
+static void LuosHAL_FlashInit(void);
+static void LuosHAL_FlashEraseLuosMemoryInfo(void);
+/////////////////////////Luos Library Needed function///////////////////////////
+
+/******************************************************************************
+ * @brief Luos HAL general initialisation
+ * @param None
+ * @return None
+ ******************************************************************************/
+void LuosHAL_Init(void)
+{
+    // Systick Initialization
+    LuosHAL_SystickInit();
+
+    // Flash Initialization
+    LuosHAL_FlashInit();
+
+    // start timestamp
+    LuosHAL_StartTimestamp();
+}
+/******************************************************************************
+ * @brief Luos HAL general disable IRQ
+ * @param None
+ * @return None
+ ******************************************************************************/
+void LuosHAL_SetIrqState(uint8_t Enable)
+{
+    if (Enable == true)
+    {
+        portENABLE_INTERRUPTS();
+    }
+    else
+    {
+        portDISABLE_INTERRUPTS();
+    }
+}
+/******************************************************************************
+ * @brief Luos HAL general systick tick at 1ms initialize
+ * @param None
+ * @return tick Counter
+ ******************************************************************************/
+static void LuosHAL_SystickInit(void)
+{
+}
+
+/******************************************************************************
+ * @brief Luos HAL general systick tick at 1ms
+ * @param None
+ * @return tick Counter
+ ******************************************************************************/
+uint32_t LuosHAL_GetSystick(void)
+{
+    return esp_timer_get_time() / 1000;
+}
+/******************************************************************************
+ * @brief Luos GetTimestamp
+ * @param None
+ * @return uint64_t
+ ******************************************************************************/
+uint64_t LuosHAL_GetTimestamp(void)
+{
+    return esp_timer_get_time() * 1000;
+}
+
+/******************************************************************************
+ * @brief Luos start Timestamp
+ * @param None
+ * @return None
+ ******************************************************************************/
+void LuosHAL_StartTimestamp(void)
+{
+}
+
+/******************************************************************************
+ * @brief Luos stop Timestamp
+ * @param None
+ * @return None
+ ******************************************************************************/
+void LuosHAL_StopTimestamp(void)
+{
+}
+
+/******************************************************************************
+ * @brief Flash Initialisation
+ * @param None
+ * @return None
+ ******************************************************************************/
+static void LuosHAL_FlashInit(void)
+{
+}
+/******************************************************************************
+ * @brief Erase flash page where Luos keep permanente information
+ * @param None
+ * @return None
+ ******************************************************************************/
+static void LuosHAL_FlashEraseLuosMemoryInfo(void)
+{
+}
+/******************************************************************************
+ * @brief Write flash page where Luos keep permanente information
+ * @param Address page / size to write / pointer to data to write
+ * @return
+ ******************************************************************************/
+void LuosHAL_FlashWriteLuosMemoryInfo(uint32_t addr, uint16_t size, uint8_t *data)
+{
+}
+/******************************************************************************
+ * @brief read information from page where Luos keep permanente information
+ * @param Address info / size to read / pointer callback data to read
+ * @return
+ ******************************************************************************/
+void LuosHAL_FlashReadLuosMemoryInfo(uint32_t addr, uint16_t size, uint8_t *data)
+{
+    // memcpy(data, (void *)(addr), size);
+}
+
+/******************************************************************************
+ * @brief Set boot mode in shared flash memory
+ * @param
+ * @return
+ ******************************************************************************/
+void LuosHAL_SetMode(uint8_t mode)
+{
+}
+
+/******************************************************************************
+ * @brief Save node ID in shared flash memory
+ * @param Address, node_id
+ * @return
+ ******************************************************************************/
+void LuosHAL_SaveNodeID(uint16_t node_id)
+{
+}
+
+/******************************************************************************
+ * @brief software reboot the microprocessor
+ * @param
+ * @return
+ ******************************************************************************/
+void LuosHAL_Reboot(void)
+{
+}
+
+#ifdef BOOTLOADER_CONFIG
+#endif
---
+++ network/robus/HAL/ESP32/robus_hal.h
@@ -0,0 +1,31 @@
+/******************************************************************************
+ * @file robus_HAL
+ * @brief Robus Hardware Abstration Layer. Describe Low layer fonction
+ * @MCU Family STM32FO
+ * @author Luos
+ * @version 0.0.0
+ ******************************************************************************/
+#ifndef _RobusHAL_H_
+#define _RobusHAL_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "robus_hal_config.h"
+
+/*******************************************************************************
+ * Function
+ ******************************************************************************/
+void RobusHAL_Init(void);
+void RobusHAL_ComInit(uint32_t Baudrate);
+void RobusHAL_SetTxState(uint8_t Enable);
+void RobusHAL_SetRxState(uint8_t Enable);
+void RobusHAL_ComTransmit(uint8_t *data, uint16_t size);
+uint8_t RobusHAL_GetTxLockState(void);
+void RobusHAL_ResetTimeout(uint16_t nbrbit);
+void RobusHAL_SetRxDetecPin(uint8_t Enable);
+void RobusHAL_SetPTPDefaultState(uint8_t PTPNbr);
+void RobusHAL_SetPTPReverseState(uint8_t PTPNbr);
+void RobusHAL_PushPTP(uint8_t PTPNbr);
+uint8_t RobusHAL_GetPTPState(uint8_t PTPNbr);
+void RobusHAL_ComputeCRC(uint8_t *data, uint8_t *crc);
+#endif /* _RobusHAL_H_ */
