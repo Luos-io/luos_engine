c8e38eb8c5fe9e3b7f8ebe0ad0dc54f00eb6c478
JeromeGalan <jerome.galan@luos.io>
2022-06-20 16:19:45 +0200

    Fix unit test for mask calculation

--- test/test_robus/main.c
+++ test/test_robus/main.c
@@ -1,54 +1,56 @@
+#include <stdio.h>
 #include "main.h"
 #include "robus.h"
 #include "context.h"
-#include <stdio.h>
+#include "unit_test.h"
 
 void unittest_Robus_IDMaskCalculation()
 {
-    NEW_TEST_CASE("ID shift mast test");
+    NEW_TEST_CASE("ID shift mask test");
     {
         Robus_MaskInit();
 
-        Robus_IDMaskCalculation(7, MAX_SERVICE_NUMBER);
+        Robus_IDMaskCalculation(7, SERVICE_NUMBER);
         TEST_ASSERT_EQUAL(0, ctx.IDShiftMask);
 
-        Robus_IDMaskCalculation(8, MAX_SERVICE_NUMBER);
+        Robus_IDMaskCalculation(8, SERVICE_NUMBER);
         TEST_ASSERT_EQUAL(0, ctx.IDShiftMask);
 
-        Robus_IDMaskCalculation(9, MAX_SERVICE_NUMBER);
+        Robus_IDMaskCalculation(9, SERVICE_NUMBER);
         TEST_ASSERT_EQUAL(1, ctx.IDShiftMask);
     }
 
-    NEW_TEST_CASE("ID mask mast test");
+    NEW_TEST_CASE("ID mask test");
     {
         Robus_MaskInit();
-
-        Robus_IDMaskCalculation(7, MAX_SERVICE_NUMBER);
+        Robus_IDMaskCalculation(7, SERVICE_NUMBER);
         TEST_ASSERT_EQUAL(0xC0, ctx.IDMask[0]);
         TEST_ASSERT_EQUAL(0x07, ctx.IDMask[1]);
 
-        Robus_IDMaskCalculation(8, MAX_SERVICE_NUMBER);
-        TEST_ASSERT_EQUAL(1, ctx.IDShiftMask);
+        Robus_MaskInit();
+        Robus_IDMaskCalculation(8, SERVICE_NUMBER);
+        TEST_ASSERT_EQUAL(0, ctx.IDShiftMask);
         TEST_ASSERT_EQUAL(0x80, ctx.IDMask[0]);
         TEST_ASSERT_EQUAL(0x0F, ctx.IDMask[1]);
 
-        Robus_IDMaskCalculation(9, MAX_SERVICE_NUMBER);
+        Robus_MaskInit();
+        Robus_IDMaskCalculation(9, SERVICE_NUMBER);
         TEST_ASSERT_EQUAL(1, ctx.IDShiftMask);
-        TEST_ASSERT_EQUAL(0x00, ctx.IDMask[0]);
-        TEST_ASSERT_EQUAL(0x1F, ctx.IDMask[1]);
+        TEST_ASSERT_EQUAL(0x1F, ctx.IDMask[0]);
+        TEST_ASSERT_EQUAL(0x00, ctx.IDMask[1]);
     }
 
     NEW_TEST_CASE("Limit 4096");
     {
         Robus_MaskInit();
-        Robus_IDMaskCalculation(4096 - MAX_SERVICE_NUMBER, MAX_SERVICE_NUMBER);
+        Robus_IDMaskCalculation(4096 - SERVICE_NUMBER, SERVICE_NUMBER);
         TEST_ASSERT_EQUAL(511, ctx.IDShiftMask);
     }
 
     NEW_TEST_CASE("Limit 0");
     {
         Robus_MaskInit();
-        Robus_IDMaskCalculation(0, MAX_SERVICE_NUMBER);
+        Robus_IDMaskCalculation(0, SERVICE_NUMBER);
         TEST_ASSERT_TRUE(IS_ASSERT());
         RESET_ASSERT();
     }
--- test/test_robus/main.h
+++ test/test_robus/main.h
@@ -1,4 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define SERVICE_NUMBER 5
+
 #endif // MAIN_H
