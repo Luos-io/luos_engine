3ed91f85a1aa33ea14de3bd334e722d97bd896cc
Salem_Luos <thomas.salembier@luos.io>
2022-07-01 17:43:13 +0200

    Add HAL for STM32L0 familly

---
+++ engine/HAL/STM32L0/luos_hal.h
@@ -0,0 +1,63 @@
+/******************************************************************************
+ * @file luosHAL
+ * @brief Luos Hardware Abstration Layer. Describe Low layer fonction
+ * @MCU Family STM32FO
+ * @author Luos
+ * @version 0.0.0
+ ******************************************************************************/
+#ifndef _LUOSHAL_H_
+#define _LUOSHAL_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <luos_hal_config.h>
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+#define LUOS_UUID ((uint32_t *)0x1FFFF7AC)
+
+#define BOOT_MODE_MASK   0x000000FF
+#define BOOT_MODE_OFFSET 0
+#define NODE_ID_MASK     0x00FFFF00
+#define NODE_ID_OFFSET   8
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+
+typedef struct ll_timestamp
+{
+    uint32_t lower_timestamp;
+    uint64_t higher_timestamp;
+    uint32_t start_offset;
+    ;
+} ll_timestamp_t;
+
+/*******************************************************************************
+ * Function
+ ******************************************************************************/
+void LuosHAL_Init(void);
+void LuosHAL_SetIrqState(uint8_t Enable);
+uint32_t LuosHAL_GetSystick(void);
+
+// bootloader functions
+void LuosHAL_SetMode(uint8_t mode);
+void LuosHAL_Reboot(void);
+void LuosHAL_SaveNodeID(uint16_t);
+void LuosHAL_JumpToAddress(uint32_t);
+uint8_t LuosHAL_GetMode(void);
+
+#ifdef BOOTLOADER
+void LuosHAL_DeInit(void);
+uint16_t LuosHAL_GetNodeID(void);
+void LuosHAL_EraseMemory(uint32_t, uint16_t);
+void LuosHAL_ProgramFlash(uint32_t, uint16_t, uint8_t *);
+#endif
+
+// timestamp functions
+uint64_t LuosHAL_GetTimestamp(void);
+void LuosHAL_StartTimestamp(void);
+void LuosHAL_StopTimestamp(void);
+
+#endif /* _LUOSHAL_H_ */
---
+++ engine/HAL/STM32L0/luos_hal_config.h
@@ -0,0 +1,69 @@
+/******************************************************************************
+ * @file luosHAL_Config
+ * @brief This file allow you to configure LuosHAL according to your design
+ *        this is the default configuration created by Luos team for this MCU Family
+ *        Do not modify this file if you want to ovewrite change define in you project
+ * @MCU Family STM32L0
+ * @author Luos
+ * @version 0.0.0
+ ******************************************************************************/
+/*
+ *
+ * WITH_BOOTLOADER MODE FLASH
+ *
+ *
+ *             SHARED_MEMORY_ADDRESS
+ *             0x0800C000
+ *               |
+ *               |   APP_START_ADDRESS                  APP_END_ADDRESS
+ *               |   |0x0800C800                          |
+ *               |   |                                    |
+ *               v   v                                    v
+ *   +-----------+---+------------------------------------+
+ *   |   48Kb    |2Kb|           ...                      |
+ *   +-----------+---+------------------------------------+
+ *   ^
+ *   |
+ *   |
+ *   |
+ * BOOT_START_ADDRESS
+ * 0x08000000
+ */
+
+#ifndef _LUOSHAL_CONFIG_H_
+#define _LUOSHAL_CONFIG_H_
+
+#include "stm32l0xx_hal.h"
+// clang-format off
+
+#ifndef MCUFREQ
+#define MCUFREQ 32000000 // MCU frequence
+#endif
+
+/*******************************************************************************
+ * BOOTLOADER CONFIG
+ ******************************************************************************/
+// The beginning of the bootloader code in flash
+#ifndef BOOT_START_ADDRESS
+#define BOOT_START_ADDRESS (uint32_t)FLASH_BASE
+#endif
+// Shared memory to store bootmode
+#ifndef SHARED_MEMORY_ADDRESS
+#define SHARED_MEMORY_ADDRESS (uint32_t)0x0800C000
+#endif
+// begining of application in flash - by default after bootloader & shared mem
+#ifndef APP_START_ADDRESS
+#define APP_START_ADDRESS           (uint32_t)0x0800D000
+#endif
+// last address of app in case of a bootloader
+#ifndef APP_END_ADDRESS
+#define APP_END_ADDRESS           (uint32_t)FLASH_END
+#endif
+// Remapping address of vector table in flash
+#ifdef WITH_BOOTLOADER
+#define LUOS_VECT_TAB APP_START_ADDRESS
+#else
+#define LUOS_VECT_TAB BOOT_START_ADDRESS
+#endif
+
+#endif /* _LUOSHAL_CONFIG_H_ */
