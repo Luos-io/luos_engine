f2a09e1fee39516224bd35fd4a683772b8fc1a30
Nicolas Rabault <nicolas.rabault@luos.io>
2022-08-10 09:49:34 +0200

    Update HAL templates with comment to simplify porting and contribution

--- engine/HAL/template/luos_hal.c
+++ engine/HAL/template/luos_hal.c
@@ -48,13 +48,27 @@ void LuosHAL_Init(void)
  ******************************************************************************/
 void LuosHAL_SetIrqState(uint8_t Enable)
 {
+    /*************************************************************************
+     *
+     * This function shutdonw all the IRQ in the MCU to make atomic execution
+     * of the part of the code when it's call
+     *
+     ************************************************************************/
     if (Enable == true)
     {
-        __enable_irq();
+        /*************************************************************************
+         *
+         * atomic exit exemple with CMSIS __enable_irq();
+         *
+         ************************************************************************/
     }
     else
     {
-        __disable_irq();
+        /*************************************************************************
+         *
+         * atomic entry exemple with CMSIS __disable_irq();
+         *
+         ************************************************************************/
     }
 }
 /******************************************************************************
@@ -64,7 +78,13 @@ void LuosHAL_SetIrqState(uint8_t Enable)
  ******************************************************************************/
 static void LuosHAL_SystickInit(void)
 {
-    // Systick timer initialization
+    /*************************************************************************
+     *
+     * This function initialize Luos systick.
+     * the Luos systick allow user to track time at 1ms since
+     * the systick has been started
+     *
+     ************************************************************************/
 }
 /******************************************************************************
  * @brief Luos HAL general systick tick at 1ms
@@ -73,9 +93,13 @@ static void LuosHAL_SystickInit(void)
  ******************************************************************************/
 uint32_t LuosHAL_GetSystick(void)
 {
+    /*************************************************************************
+     *
+     * This function return the value of Luos systick since has been started
+     *
+     ************************************************************************/
     return; // return  tick
 }
-
 /******************************************************************************
  * @brief Luos GetTimestamp
  * @param None
@@ -83,8 +107,16 @@ uint32_t LuosHAL_GetSystick(void)
  ******************************************************************************/
 uint64_t LuosHAL_GetTimestamp(void)
 {
-    // Get a ns time measurement for timestamp value
-    // You prabobly can use the systick timer to get it
+
+    /*************************************************************************
+     *
+     * This function give a ns time measurement for timestamp value
+     * you can create the timestamp value with your own timer or by
+     * reading the value of the systick register
+     *
+     * this timestamps value will be as precise as the MCU frequency is high
+     *
+     ************************************************************************/
 
     return // Your timestamp value;
 }
@@ -96,9 +128,17 @@ uint64_t LuosHAL_GetTimestamp(void)
  ******************************************************************************/
 void LuosHAL_StartTimestamp(void)
 {
-    // Set the ll_timestamp.start_offset value to the actual timestamp value
-    // If you are using your systick timer you can simply do
-    ll_timestamp.start_offset = LuosHAL_GetSystick();
+    /*************************************************************************
+     *
+     * This function Set the ll_timestamp.start_offset value to the actual
+     * timestamp value
+     *
+     * the timestamp value is an int64
+     *
+     ************************************************************************/
+    // set ll_timestamp.lower_timestamp
+    // set ll_timestamp.higher_timestamp
+    return ll_timestamp.higher_timestamp * 1000000 + (uint64_t)ll_timestamp.lower_timestamp;
 }
 
 /******************************************************************************
@@ -108,16 +148,13 @@ void LuosHAL_StartTimestamp(void)
  ******************************************************************************/
 void LuosHAL_StopTimestamp(void)
 {
+    /*************************************************************************
+     *
+     * This function Reset your timestamp struct to
+     *
+     ************************************************************************/
     // Reset your timestamp struct
     ll_timestamp.lower_timestamp  = 0;
     ll_timestamp.higher_timestamp = 0;
     ll_timestamp.start_offset     = 0;
-}
-/******************************************************************************
- * @brief Flash Initialisation
- * @param None
- * @return None
- ******************************************************************************/
-static void LuosHAL_FlashInit(void)
-{
 }
