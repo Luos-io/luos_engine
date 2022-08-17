71d5c25320622c5f8c51a2fa3f5297746582b158
Salem_Luos <thomas.salembier@luos.io>
2022-08-17 19:22:44 +0200

    clean bootloader project of unused pinout for all family

--- examples/projects/STM32F4-discovery/bootloader/include/main.h
+++ examples/projects/STM32F4-discovery/bootloader/include/main.h
@@ -1,35 +1,35 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.h
-  * @brief          : Header for main.c file.
-  *                   This file contains the common defines of the application.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.h
+ * @brief          : Header for main.c file.
+ *                   This file contains the common defines of the application.
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __MAIN_H
-#define __MAIN_H
+    #define __MAIN_H
 
-#ifdef __cplusplus
+    #ifdef __cplusplus
 extern "C"
 {
-#endif
+    #endif
 
-/* Includes ------------------------------------------------------------------*/
-#include "stm32f4xx_hal.h"
+    /* Includes ------------------------------------------------------------------*/
+    #include "stm32f4xx_hal.h"
 
     /* Private includes ----------------------------------------------------------*/
     /* USER CODE BEGIN Includes */
@@ -54,22 +54,18 @@ extern "C"
     /* Exported functions prototypes ---------------------------------------------*/
     void Error_Handler(void);
 
-/* USER CODE BEGIN EFP */
+    /* USER CODE BEGIN EFP */
 
-/* USER CODE END EFP */
+    /* USER CODE END EFP */
 
-/* Private defines -----------------------------------------------------------*/
-#define BTN_Pin       GPIO_PIN_0
-#define BTN_GPIO_Port GPIOA
-#define LED_Pin       GPIO_PIN_12
-#define LED_GPIO_Port GPIOD
+    /* Private defines -----------------------------------------------------------*/
     /* USER CODE BEGIN Private defines */
 
     /* USER CODE END Private defines */
 
-#ifdef __cplusplus
+    #ifdef __cplusplus
 }
-#endif
+    #endif
 
 #endif /* __MAIN_H */
 
