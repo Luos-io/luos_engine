156a52256b5d4e5f343f19c93a9d49e0970222a2
Salem_Luos <thomas.salembier@luos.io>
2022-08-22 13:56:44 +0200

    clean unused api and benchmark in gate

--- network/robus/inc/robus.h
+++ network/robus/inc/robus.h
@@ -36,7 +36,6 @@ error_return_t Robus_SetTxTask(ll_service_t *ll_service, msg_t *msg);
 error_return_t Robus_SendMsg(ll_service_t *ll_service, msg_t *msg);
 uint16_t Robus_TopologyDetection(ll_service_t *ll_service);
 node_t *Robus_GetNode(void);
-void Robus_Flush(void);
 void Robus_IDMaskCalculation(uint16_t service_id, uint16_t service_number);
 void Robus_SetNodeDetected(network_state_t);
 network_state_t Robus_IsNodeDetected(void);
