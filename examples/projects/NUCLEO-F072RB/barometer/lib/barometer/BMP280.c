a7ad90d03f42d547e1b83a363626c0f4efcd5eaa
JeromeGalan <jerome.galan@luos.io>
2022-08-23 11:18:18 +0200

    Linting corrections for barometer

--- examples/projects/NUCLEO-F072RB/barometer/lib/barometer/BMP280.c
+++ examples/projects/NUCLEO-F072RB/barometer/lib/barometer/BMP280.c
@@ -1,66 +1,66 @@
 #include "BMP280.h"
 
 /* Registers */
-uint8_t Sub1_Temperature1_Reg = 0x88;
-uint8_t Sub2_Temperature1_Reg = 0x89;
-uint8_t Sub1_Temperature2_Reg = 0x8A;
-uint8_t Sub2_Temperature2_Reg = 0x8B;
-uint8_t Sub1_Temperature3_Reg = 0x8C;
-uint8_t Sub2_Temperature3_Reg = 0x8D;
+uint8_t Sub1_Temperature1_Reg             = 0x88;
+uint8_t Sub2_Temperature1_Reg             = 0x89;
+uint8_t Sub1_Temperature2_Reg             = 0x8A;
+uint8_t Sub2_Temperature2_Reg             = 0x8B;
+uint8_t Sub1_Temperature3_Reg             = 0x8C;
+uint8_t Sub2_Temperature3_Reg             = 0x8D;
 uint8_t Sub1_Compensation_Temperature_Reg = 0xFC;
 uint8_t Sub2_Compensation_Temperature_Reg = 0xFB;
 uint8_t Sub3_Compensation_Temperature_Reg = 0xFA;
 
-uint8_t Sub1_Pressure1_Reg = 0x8E;
-uint8_t Sub2_Pressure1_Reg = 0x8F;
-uint8_t Sub1_Pressure2_Reg = 0x90;
-uint8_t Sub2_Pressure2_Reg = 0x91;
-uint8_t Sub1_Pressure3_Reg = 0x92;
-uint8_t Sub2_Pressure3_Reg = 0x93;
-uint8_t Sub1_Pressure4_Reg = 0x94;
-uint8_t Sub2_Pressure4_Reg = 0x95;
-uint8_t Sub1_Pressure5_Reg = 0x96;
-uint8_t Sub2_Pressure5_Reg = 0x97;
-uint8_t Sub1_Pressure6_Reg = 0x98;
-uint8_t Sub2_Pressure6_Reg = 0x99;
-uint8_t Sub1_Pressure7_Reg = 0x9A;
-uint8_t Sub2_Pressure7_Reg = 0x9B;
-uint8_t Sub1_Pressure8_Reg = 0x9C;
-uint8_t Sub2_Pressure8_Reg = 0x9D;
-uint8_t Sub1_Pressure9_Reg = 0x9E;
-uint8_t Sub2_Pressure9_Reg = 0x9F;
+uint8_t Sub1_Pressure1_Reg             = 0x8E;
+uint8_t Sub2_Pressure1_Reg             = 0x8F;
+uint8_t Sub1_Pressure2_Reg             = 0x90;
+uint8_t Sub2_Pressure2_Reg             = 0x91;
+uint8_t Sub1_Pressure3_Reg             = 0x92;
+uint8_t Sub2_Pressure3_Reg             = 0x93;
+uint8_t Sub1_Pressure4_Reg             = 0x94;
+uint8_t Sub2_Pressure4_Reg             = 0x95;
+uint8_t Sub1_Pressure5_Reg             = 0x96;
+uint8_t Sub2_Pressure5_Reg             = 0x97;
+uint8_t Sub1_Pressure6_Reg             = 0x98;
+uint8_t Sub2_Pressure6_Reg             = 0x99;
+uint8_t Sub1_Pressure7_Reg             = 0x9A;
+uint8_t Sub2_Pressure7_Reg             = 0x9B;
+uint8_t Sub1_Pressure8_Reg             = 0x9C;
+uint8_t Sub2_Pressure8_Reg             = 0x9D;
+uint8_t Sub1_Pressure9_Reg             = 0x9E;
+uint8_t Sub2_Pressure9_Reg             = 0x9F;
 uint8_t Sub1_Compensation_Pressure_Reg = 0xF9;
 uint8_t Sub2_Compensation_Pressure_Reg = 0xF8;
 uint8_t Sub3_Compensation_Pressure_Reg = 0xF7;
 
-uint8_t Reset_Register = 0x60;
+uint8_t Reset_Register         = 0x60;
 uint8_t W_Config_meas_Register = 0x74;
 uint8_t R_Config_meas_Register = 0xF4;
-uint8_t W_Config_Register = 0x75;
-uint8_t R_Config_Register = 0xF5;
-uint8_t Status_Register = 0xF3;
+uint8_t W_Config_Register      = 0x75;
+uint8_t R_Config_Register      = 0xF5;
+uint8_t Status_Register        = 0xF3;
 
 /* Initialization */
 uint8_t Reset_Command = 0xB6;
-uint8_t Config_meas = 0x57;
-uint8_t Config = 0x90;
-uint8_t Status = 0;
+uint8_t Config_meas   = 0x57;
+uint8_t Config        = 0x90;
+uint8_t Status        = 0;
 
 /* Data needed for calculation */
-uint16_t Temperature1 = 0;
-int16_t Temperature2 = 0;
-int16_t Temperature3 = 0;
+uint16_t Temperature1            = 0;
+int16_t Temperature2             = 0;
+int16_t Temperature3             = 0;
 int32_t Compensation_Temperature = 0;
-uint16_t Pressure1 = 0;
-int16_t Pressure2 = 0;
-int16_t Pressure3 = 0;
-int16_t Pressure4 = 0;
-int16_t Pressure5 = 0;
-int16_t Pressure6 = 0;
-int16_t Pressure7 = 0;
-int16_t Pressure8 = 0;
-int16_t Pressure9 = 0;
-int32_t Compensation_Pressure = 0;
+uint16_t Pressure1               = 0;
+int16_t Pressure2                = 0;
+int16_t Pressure3                = 0;
+int16_t Pressure4                = 0;
+int16_t Pressure5                = 0;
+int16_t Pressure6                = 0;
+int16_t Pressure7                = 0;
+int16_t Pressure8                = 0;
+int16_t Pressure9                = 0;
+int32_t Compensation_Pressure    = 0;
 
 /* Temporary storage */
 uint8_t Sub_Data1;
@@ -68,23 +68,23 @@ uint8_t Sub_Data2;
 uint8_t Sub_Data3;
 
 /* Calculation */
-float Var1_T = 0;
-float Var2_T = 0;
-float t_fine = 0;
-float Var11_P = 0;
-float Var12_P = 0;
-float Var13_P = 0;
-float Var14_P = 0;
-float Var21_P = 0;
-float Var22_P = 0;
-float Var23_P = 0;
-float Var24_P = 0;
+float Var1_T         = 0;
+float Var2_T         = 0;
+float t_fine         = 0;
+float Var11_P        = 0;
+float Var12_P        = 0;
+float Var13_P        = 0;
+float Var14_P        = 0;
+float Var21_P        = 0;
+float Var22_P        = 0;
+float Var23_P        = 0;
+float Var24_P        = 0;
 float Pressure_temp1 = 0;
 float Pressure_temp2 = 0;
 
 /* Final Data */
 float Temperature = 0;
-float Pressure = 0;
+float Pressure    = 0;
 
 static void BMP280_MsgHandler(service_t *service, msg_t *msg);
 
@@ -94,10 +94,10 @@ static void BMP280_MsgHandler(service_t *service, msg_t *msg)
     if (msg->header.cmd == GET_CMD)
     {
         msg_t pub_msg;
-        pub_msg.header.cmd = PRESSURE;
+        pub_msg.header.cmd         = PRESSURE;
         pub_msg.header.target_mode = ID;
-        pub_msg.header.target = msg->header.source;
-        pub_msg.header.size = sizeof(pressure_t);
+        pub_msg.header.target      = msg->header.source;
+        pub_msg.header.size        = sizeof(pressure_t);
         memcpy(pub_msg.data, &Pressure, sizeof(pressure_t));
         /* Comment this line when using Pyluos to simulate pressure evolution */
         Luos_SendMsg(service, &pub_msg);
@@ -105,10 +105,10 @@ static void BMP280_MsgHandler(service_t *service, msg_t *msg)
     if (msg->header.cmd == PARAMETERS)
     {
         msg_t pub_msg;
-        pub_msg.header.cmd = PRESSURE;
+        pub_msg.header.cmd         = PRESSURE;
         pub_msg.header.target_mode = ID;
-        pub_msg.header.target = msg->header.source;
-        pub_msg.header.size = sizeof(pressure_t);
+        pub_msg.header.target      = msg->header.source;
+        pub_msg.header.size        = sizeof(pressure_t);
         PressureOD_PressureToMsg(&Pressure, &pub_msg);
         Luos_SendMsg(service, &pub_msg);
         return;
@@ -187,9 +187,9 @@ void BMP280_Loop()
     Compensation_Temperature = Compensation_Temperature | Sub_Data3 << 12;
 
     /* Determination of temperature with several calculations according to documentation */
-    Var1_T = (((float)Compensation_Temperature / (float)16384) - ((float)Temperature1 / (float)1024)) * (float)Temperature2;
-    Var2_T = (((float)Compensation_Temperature / (float)131072) - (float)Temperature1 / (float)8192) * (((float)Compensation_Temperature / (float)131072) - (float)Temperature1 / (float)8192) * (float)Temperature3;
-    t_fine = Var1_T + Var2_T;
+    Var1_T      = (((float)Compensation_Temperature / (float)16384) - ((float)Temperature1 / (float)1024)) * (float)Temperature2;
+    Var2_T      = (((float)Compensation_Temperature / (float)131072) - (float)Temperature1 / (float)8192) * (((float)Compensation_Temperature / (float)131072) - (float)Temperature1 / (float)8192) * (float)Temperature3;
+    t_fine      = Var1_T + Var2_T;
     Temperature = (Var1_T + Var2_T) / 5120;
 
     /* Same process for Pressure */
@@ -275,15 +275,15 @@ void BMP280_Loop()
     Compensation_Pressure = Compensation_Pressure | Sub_Data2 << 4;
     Compensation_Pressure = Compensation_Pressure | Sub_Data3 << 12;
 
-    Var11_P = ((float)t_fine / 2) - 64000;
-    Var21_P = Var11_P * Var11_P * ((float)Pressure6) / 32768;
-    Var22_P = Var21_P + Var11_P * ((float)Pressure5) * 2;
-    Var23_P = (Var22_P / 4) + (((float)Pressure4) * 65536);
-    Var12_P = (((float)Pressure3) * Var11_P * Var11_P / 524288 + ((float)Pressure2) * Var11_P) / 524288;
-    Var13_P = (1 + Var12_P / 32768) * ((float)Pressure1);
+    Var11_P        = ((float)t_fine / 2) - 64000;
+    Var21_P        = Var11_P * Var11_P * ((float)Pressure6) / 32768;
+    Var22_P        = Var21_P + Var11_P * ((float)Pressure5) * 2;
+    Var23_P        = (Var22_P / 4) + (((float)Pressure4) * 65536);
+    Var12_P        = (((float)Pressure3) * Var11_P * Var11_P / 524288 + ((float)Pressure2) * Var11_P) / 524288;
+    Var13_P        = (1 + Var12_P / 32768) * ((float)Pressure1);
     Pressure_temp1 = 1048576 - (float)Compensation_Pressure;
     Pressure_temp2 = (Pressure_temp1 - (Var23_P / 4096)) * 6250 / Var13_P;
-    Var14_P = ((float)Pressure9) * Pressure_temp2 * Pressure_temp2 / 2147483648;
-    Var24_P = Pressure_temp2 * ((float)Pressure8) / 32768;
-    Pressure = Pressure_temp2 + (Var14_P + Var24_P + ((float)Pressure7)) / 16;
+    Var14_P        = ((float)Pressure9) * Pressure_temp2 * Pressure_temp2 / 2147483648;
+    Var24_P        = Pressure_temp2 * ((float)Pressure8) / 32768;
+    Pressure       = Pressure_temp2 + (Var14_P + Var24_P + ((float)Pressure7)) / 16;
 }
