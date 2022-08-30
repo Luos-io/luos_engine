e511d10b491f8408651c0f87088df157e0279d02
biliouriv <biliouriv@gmail.com>
2022-08-30 18:34:12 +0200

    [Gate] Multicast bootloader messages

--- tool_services/gate/TinyJSON/bootloader_ex.c
+++ tool_services/gate/TinyJSON/bootloader_ex.c
@@ -10,6 +10,7 @@
 #include "bootloader_core.h"
 #include "gate_config.h"
 #include "pipe_link.h"
+#include "routing_table.h"
 
 /*******************************************************************************
  * Definitions
@@ -31,31 +32,31 @@ void Bootloader_LuosToJson(service_t *service, msg_t *msg)
 {
     char boot_json[64]   = "\0";
     uint8_t response_cmd = msg->data[0];
-
+    uint16_t node_id     = RoutingTB_NodeIDFromID(msg->header.source);
     switch (response_cmd)
     {
         case BOOTLOADER_READY_RESP:
-            sprintf(boot_json, "{\"bootloader\":{\"response\":%d}}\n", BOOTLOADER_READY_RESP);
+            sprintf(boot_json, "{\"bootloader\":{\"response\":%d, \"node\":%d}}\n", BOOTLOADER_READY_RESP, node_id);
             break;
 
         case BOOTLOADER_ERASE_RESP:
-            sprintf(boot_json, "{\"bootloader\":{\"response\":%d}}\n", BOOTLOADER_ERASE_RESP);
+            sprintf(boot_json, "{\"bootloader\":{\"response\":%d, \"node\":%d}}\n", BOOTLOADER_ERASE_RESP, node_id);
             break;
 
         case BOOTLOADER_BIN_CHUNK_RESP:
-            sprintf(boot_json, "{\"bootloader\":{\"response\":%d}}\n", BOOTLOADER_BIN_CHUNK_RESP);
+            sprintf(boot_json, "{\"bootloader\":{\"response\":%d, \"node\":%d}}\n", BOOTLOADER_BIN_CHUNK_RESP, node_id);
             break;
 
         case BOOTLOADER_BIN_END_RESP:
-            sprintf(boot_json, "{\"bootloader\":{\"response\":%d}}\n", BOOTLOADER_BIN_END_RESP);
+            sprintf(boot_json, "{\"bootloader\":{\"response\":%d, \"node\":%d}}\n", BOOTLOADER_BIN_END_RESP, node_id);
             break;
 
         case BOOTLOADER_CRC_RESP:
-            sprintf(boot_json, "{\"bootloader\":{\"response\":%d,\"crc_value\":%d}}\n", BOOTLOADER_CRC_RESP, msg->data[1]);
+            sprintf(boot_json, "{\"bootloader\":{\"response\":%d,\"crc_value\":%d, \"node\":%d}}\n", BOOTLOADER_CRC_RESP, msg->data[1], node_id);
             break;
 
         case BOOTLOADER_ERROR_SIZE:
-            sprintf(boot_json, "{\"bootloader\":{\"response\":%d}}\n", BOOTLOADER_ERROR_SIZE);
+            sprintf(boot_json, "{\"bootloader\":{\"response\":%d, \"node\":%d}}\n", BOOTLOADER_ERROR_SIZE, node_id);
             break;
 
         default:
@@ -78,31 +79,34 @@ void Bootloader_JsonToLuos(service_t *service, char *bin_data, json_t const *boo
         // get "command" json object
         json_t const *command_item = json_getProperty(bootloader_json, "command");
         // parse all relevant values in json object
-        uint16_t type       = (uint16_t)json_getReal(json_getProperty(command_item, "type"));
-        uint8_t node_target = (uint8_t)json_getReal(json_getProperty(command_item, "node"));
-
+        uint16_t type        = (uint16_t)json_getReal(json_getProperty(command_item, "type"));
+        uint8_t topic_target = (uint8_t)json_getReal(json_getProperty(command_item, "topic"));
         // create a message to send to nodes
         msg_t boot_msg;
-        boot_msg.header.target      = node_target;    // first node of the network
+        boot_msg.header.target      = topic_target;   // first node of the network
         boot_msg.header.cmd         = BOOTLOADER_CMD; // bootloader cmd
-        boot_msg.header.target_mode = NODEIDACK;      // msg send to the node
+        boot_msg.header.target_mode = TOPIC;          // msg send to the node
 
         uint32_t binary_size = 0;
         json_t *item         = NULL;
         switch (type)
         {
             case BOOTLOADER_START:
-                // send start command to bootloader app
-                boot_msg.header.size = sizeof(char);
-                boot_msg.data[0]     = BOOTLOADER_START;
+                boot_msg.header.target_mode = NODEIDACK;
+                boot_msg.header.target      = (uint16_t)json_getReal(json_getProperty(command_item, "node"));
+                boot_msg.header.size        = sizeof(char);
+                boot_msg.data[0]            = BOOTLOADER_START;
                 Luos_SendMsg(service, &boot_msg);
                 break;
 
             case BOOTLOADER_STOP:
                 // send stop command to bootloader app
-                boot_msg.header.size = sizeof(char);
-                boot_msg.data[0]     = BOOTLOADER_STOP;
+                boot_msg.header.target_mode = NODEIDACK;
+                boot_msg.header.target      = (uint16_t)json_getReal(json_getProperty(command_item, "node"));
+                boot_msg.header.size        = sizeof(char);
+                boot_msg.data[0]            = BOOTLOADER_STOP;
                 Luos_SendMsg(service, &boot_msg);
+
                 break;
 
             case BOOTLOADER_READY:
@@ -110,9 +114,12 @@ void Bootloader_JsonToLuos(service_t *service, char *bin_data, json_t const *boo
                 binary_size = (uint32_t)json_getReal(json_getProperty(command_item, "size"));
 
                 // send ready command to bootloader app
-                boot_msg.header.size = sizeof(char) + sizeof(uint32_t);
-                boot_msg.data[0]     = BOOTLOADER_READY;
-                memcpy(&(boot_msg.data[1]), &binary_size, sizeof(uint32_t));
+                boot_msg.header.target_mode = NODEIDACK;
+                boot_msg.header.target      = (uint16_t)json_getReal(json_getProperty(command_item, "node"));
+                boot_msg.header.size        = 2 * sizeof(char) + sizeof(uint32_t);
+                boot_msg.data[0]            = BOOTLOADER_READY;
+                boot_msg.data[1]            = topic_target;
+                memcpy(&(boot_msg.data[2]), &binary_size, sizeof(uint32_t));
                 Luos_SendMsg(service, &boot_msg);
                 break;
 
