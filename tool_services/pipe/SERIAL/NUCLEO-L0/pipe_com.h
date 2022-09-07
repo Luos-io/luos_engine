cadeba3bdce470a1ed43d0ee20bec170574e11a3
Salem_Luos <thomas.salembier@luos.io>
2022-09-07 12:10:31 +0200

    Add PIPE_CONFIG to modify Pipe pinout

--- tool_services/pipe/SERIAL/NUCLEO-L0/pipe_com.h
+++ tool_services/pipe/SERIAL/NUCLEO-L0/pipe_com.h
@@ -19,36 +19,41 @@
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
-#define PIPE_TX_CLK() __HAL_RCC_GPIOA_CLK_ENABLE()
-#define PIPE_TX_PIN   GPIO_PIN_2
-#define PIPE_TX_PORT  GPIOA
-#define PIPE_TX_AF    GPIO_AF4_USART2
+// PIPE_CONFIG need to be define in your node_config.h if you want to redefine
+// Pipe configuration usart or DMA If you need change something in this configuration
+// you must redefine all the configuration in you node_config.h
+#ifndef PIPE_CONFIG
+    #define PIPE_TX_CLK() __HAL_RCC_GPIOA_CLK_ENABLE()
+    #define PIPE_TX_PIN   GPIO_PIN_2
+    #define PIPE_TX_PORT  GPIOA
+    #define PIPE_TX_AF    GPIO_AF4_USART2
 
-#define PIPE_RX_CLK() __HAL_RCC_GPIOA_CLK_ENABLE()
-#define PIPE_RX_PIN   GPIO_PIN_3
-#define PIPE_RX_PORT  GPIOA
-#define PIPE_RX_AF    GPIO_AF4_USART2
+    #define PIPE_RX_CLK() __HAL_RCC_GPIOA_CLK_ENABLE()
+    #define PIPE_RX_PIN   GPIO_PIN_3
+    #define PIPE_RX_PORT  GPIOA
+    #define PIPE_RX_AF    GPIO_AF4_USART2
 
-#define PIPE_COM_CLOCK_ENABLE() __HAL_RCC_USART2_CLK_ENABLE()
-#define PIPE_COM                USART2
-#define PIPE_COM_IRQ            USART2_IRQn
-#define PIPE_COM_IRQHANDLER()   USART2_IRQHandler()
+    #define PIPE_COM_CLOCK_ENABLE() __HAL_RCC_USART2_CLK_ENABLE()
+    #define PIPE_COM                USART2
+    #define PIPE_COM_IRQ            USART2_IRQn
+    #define PIPE_COM_IRQHANDLER()   USART2_IRQHandler()
 
-#define PIPE_RX_DMA_CLOCK_ENABLE()        __HAL_RCC_DMA1_CLK_ENABLE()
-#define PIPE_RX_DMA                       DMA1
-#define PIPE_RX_DMA_CHANNEL               LL_DMA_CHANNEL_6
-#define PIPE_RX_DMA_REQUEST               LL_DMA_REQUEST_4
-#define PIPE_RX_DMA_TC(PIPE_RX_DMA)       LL_DMA_IsActiveFlag_TC6(PIPE_RX_DMA)
-#define PIPE_RX_DMA_CLEAR_TC(PIPE_RX_DMA) LL_DMA_ClearFlag_TC6(PIPE_RX_DMA)
+    #define PIPE_RX_DMA_CLOCK_ENABLE()        __HAL_RCC_DMA1_CLK_ENABLE()
+    #define PIPE_RX_DMA                       DMA1
+    #define PIPE_RX_DMA_CHANNEL               LL_DMA_CHANNEL_6
+    #define PIPE_RX_DMA_REQUEST               LL_DMA_REQUEST_4
+    #define PIPE_RX_DMA_TC(PIPE_RX_DMA)       LL_DMA_IsActiveFlag_TC6(PIPE_RX_DMA)
+    #define PIPE_RX_DMA_CLEAR_TC(PIPE_RX_DMA) LL_DMA_ClearFlag_TC6(PIPE_RX_DMA)
 
-#define PIPE_TX_DMA_CLOCK_ENABLE()        __HAL_RCC_DMA1_CLK_ENABLE()
-#define PIPE_TX_DMA                       DMA1
-#define PIPE_TX_DMA_CHANNEL               LL_DMA_CHANNEL_7
-#define PIPE_TX_DMA_REQUEST               LL_DMA_REQUEST_4
-#define PIPE_TX_DMA_TC(PIPE_TX_DMA)       LL_DMA_IsActiveFlag_TC7(PIPE_TX_DMA)
-#define PIPE_TX_DMA_CLEAR_TC(PIPE_TX_DMA) LL_DMA_ClearFlag_TC7(PIPE_TX_DMA)
-#define PIPE_TX_DMA_IRQ                   DMA1_Channel4_5_6_7_IRQn
-#define PIPE_TX_DMA_IRQHANDLER()          DMA1_Channel4_5_6_7_IRQHandler()
+    #define PIPE_TX_DMA_CLOCK_ENABLE()        __HAL_RCC_DMA1_CLK_ENABLE()
+    #define PIPE_TX_DMA                       DMA1
+    #define PIPE_TX_DMA_CHANNEL               LL_DMA_CHANNEL_7
+    #define PIPE_TX_DMA_REQUEST               LL_DMA_REQUEST_4
+    #define PIPE_TX_DMA_TC(PIPE_TX_DMA)       LL_DMA_IsActiveFlag_TC7(PIPE_TX_DMA)
+    #define PIPE_TX_DMA_CLEAR_TC(PIPE_TX_DMA) LL_DMA_ClearFlag_TC7(PIPE_TX_DMA)
+    #define PIPE_TX_DMA_IRQ                   DMA1_Channel4_5_6_7_IRQn
+    #define PIPE_TX_DMA_IRQHANDLER()          DMA1_Channel4_5_6_7_IRQHandler()
+#endif
 
 /*******************************************************************************
  * Variables
--- tool_services/pipe/SERIAL/l0/pipe_com.h
+++ tool_services/pipe/SERIAL/l0/pipe_com.h
@@ -19,36 +19,42 @@
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
-#define PIPE_TX_CLK() __HAL_RCC_GPIOB_CLK_ENABLE()
-#define PIPE_TX_PIN   GPIO_PIN_10
-#define PIPE_TX_PORT  GPIOB
-#define PIPE_TX_AF    GPIO_AF4_USART3
 
-#define PIPE_RX_CLK() __HAL_RCC_GPIOB_CLK_ENABLE()
-#define PIPE_RX_PIN   GPIO_PIN_11
-#define PIPE_RX_PORT  GPIOB
-#define PIPE_RX_AF    GPIO_AF4_USART3
+// PIPE_CONFIG need to be define in your node_config.h if you want to redefine
+// Pipe configuration usart or DMA If you need change something in this configuration
+// you must redefine all the configuration in you node_config.h
+#ifndef PIPE_CONFIG
+    #define PIPE_TX_CLK() __HAL_RCC_GPIOB_CLK_ENABLE()
+    #define PIPE_TX_PIN   GPIO_PIN_10
+    #define PIPE_TX_PORT  GPIOB
+    #define PIPE_TX_AF    GPIO_AF4_USART3
 
-#define PIPE_COM_CLOCK_ENABLE() __HAL_RCC_USART3_CLK_ENABLE()
-#define PIPE_COM                USART3
-#define PIPE_COM_IRQ            USART3_4_IRQn
-#define PIPE_COM_IRQHANDLER()   USART3_4_IRQHandler()
+    #define PIPE_RX_CLK() __HAL_RCC_GPIOB_CLK_ENABLE()
+    #define PIPE_RX_PIN   GPIO_PIN_11
+    #define PIPE_RX_PORT  GPIOB
+    #define PIPE_RX_AF    GPIO_AF4_USART3
 
-#define PIPE_RX_DMA_CLOCK_ENABLE()        __HAL_RCC_DMA1_CLK_ENABLE()
-#define PIPE_RX_DMA                       DMA1
-#define PIPE_RX_DMA_CHANNEL               LL_DMA_CHANNEL_6
-#define PIPE_RX_DMA_REQUEST               LL_SYSCFG_USART3_RMP_DMA1CH67
-#define PIPE_RX_DMA_TC(PIPE_RX_DMA)       LL_DMA_IsActiveFlag_TC6(PIPE_RX_DMA)
-#define PIPE_RX_DMA_CLEAR_TC(PIPE_RX_DMA) LL_DMA_ClearFlag_TC6(PIPE_RX_DMA)
+    #define PIPE_COM_CLOCK_ENABLE() __HAL_RCC_USART3_CLK_ENABLE()
+    #define PIPE_COM                USART3
+    #define PIPE_COM_IRQ            USART3_4_IRQn
+    #define PIPE_COM_IRQHANDLER()   USART3_4_IRQHandler()
 
-#define PIPE_TX_DMA_CLOCK_ENABLE()        __HAL_RCC_DMA1_CLK_ENABLE()
-#define PIPE_TX_DMA                       DMA1
-#define PIPE_TX_DMA_CHANNEL               LL_DMA_CHANNEL_7
-#define PIPE_TX_DMA_REQUEST               LL_SYSCFG_USART3_RMP_DMA1CH67
-#define PIPE_TX_DMA_TC(PIPE_TX_DMA)       LL_DMA_IsActiveFlag_TC7(PIPE_TX_DMA)
-#define PIPE_TX_DMA_CLEAR_TC(PIPE_TX_DMA) LL_DMA_ClearFlag_TC7(PIPE_TX_DMA)
-#define PIPE_TX_DMA_IRQ                   DMA1_Channel4_5_6_7_IRQn
-#define PIPE_TX_DMA_IRQHANDLER()          DMA1_Channel4_5_6_7_IRQHandler()
+    #define PIPE_RX_DMA_CLOCK_ENABLE()        __HAL_RCC_DMA1_CLK_ENABLE()
+    #define PIPE_RX_DMA                       DMA1
+    #define PIPE_RX_DMA_CHANNEL               LL_DMA_CHANNEL_6
+    #define PIPE_RX_DMA_REQUEST               LL_SYSCFG_USART3_RMP_DMA1CH67
+    #define PIPE_RX_DMA_TC(PIPE_RX_DMA)       LL_DMA_IsActiveFlag_TC6(PIPE_RX_DMA)
+    #define PIPE_RX_DMA_CLEAR_TC(PIPE_RX_DMA) LL_DMA_ClearFlag_TC6(PIPE_RX_DMA)
+
+    #define PIPE_TX_DMA_CLOCK_ENABLE()        __HAL_RCC_DMA1_CLK_ENABLE()
+    #define PIPE_TX_DMA                       DMA1
+    #define PIPE_TX_DMA_CHANNEL               LL_DMA_CHANNEL_7
+    #define PIPE_TX_DMA_REQUEST               LL_SYSCFG_USART3_RMP_DMA1CH67
+    #define PIPE_TX_DMA_TC(PIPE_TX_DMA)       LL_DMA_IsActiveFlag_TC7(PIPE_TX_DMA)
+    #define PIPE_TX_DMA_CLEAR_TC(PIPE_TX_DMA) LL_DMA_ClearFlag_TC7(PIPE_TX_DMA)
+    #define PIPE_TX_DMA_IRQ                   DMA1_Channel4_5_6_7_IRQn
+    #define PIPE_TX_DMA_IRQHANDLER()          DMA1_Channel4_5_6_7_IRQHandler()
+#endif
 
 /*******************************************************************************
  * Variables
