f4090bb1ebd1491fc49130cedd8d2499e80de4b4
Salem_Luos <thomas.salembier@luos.io>
2022-10-13 17:52:46 +0200

    Update button project

--- examples/projects/ESP32/button/lib/button/button.c
+++ examples/projects/ESP32/button/lib/button/button.c
@@ -32,6 +32,14 @@ void Button_Init(void)
 {
     gpio_config_t btnConfig;
 
+    gpio_reset_pin(BTN_GPIO);
+    btnConfig.intr_type    = GPIO_INTR_DISABLE;
+    btnConfig.mode         = GPIO_MODE_INPUT;
+    btnConfig.pin_bit_mask = (1ULL << BTN_GPIO);
+    btnConfig.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    btnConfig.pull_up_en   = GPIO_PULLUP_DISABLE;
+    gpio_config(&btnConfig);
+
     revision_t revision = {.major = 1, .minor = 0, .build = 0};
     Luos_CreateService(Button_MsgHandler, STATE_TYPE, "button", revision);
 }
@@ -56,7 +64,7 @@ static void Button_MsgHandler(service_t *service, msg_t *msg)
         // fill the message infos
         msg_t pub_msg;
         pub_msg.header.cmd         = IO_STATE;
-        pub_msg.header.target_mode = SERVICEID;
+        pub_msg.header.target_mode = msg->header.target_mode;
         pub_msg.header.target      = msg->header.source;
         pub_msg.header.size        = sizeof(char);
         pub_msg.data[0]            = gpio_get_level(BTN_GPIO);
