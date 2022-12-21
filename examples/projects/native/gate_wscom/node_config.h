8a503fb5563e5774d4c8a65b148a96a351f0c284
Nicolas Rabault <nicolas.rabault@luos.io>
2022-12-21 10:58:07 +0100

    [Native] Remove the button package from Gate_wscom example

--- examples/projects/native/gate_wscom/node_config.h
+++ examples/projects/native/gate_wscom/node_config.h
@@ -43,9 +43,9 @@
  *    NBR_PORT              |              2             | PTP Branch number Max 8
  *    NBR_RETRY             |              10            | Send Retry number in case of NACK or collision
  ******************************************************************************/
-#define MAX_SERVICE_NUMBER 3
+#define MAX_SERVICE_NUMBER 2
 #define MAX_PROFILE_NUMBER 1
-#define MAX_MSG_NB         5
+#define MAX_MSG_NB         30
 #define INIT_TIME          150
 
 /*******************************************************************************
