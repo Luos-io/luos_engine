081a6b0762237db2ce6080205973d588cd70ee3c
Nicolas Rabault <nicolas.rabault@luos.io>
2023-01-06 15:39:29 +0100

    Multi task services on the GateWS example

--- examples/projects/native/gate_wscom/src/main.c
+++ examples/projects/native/gate_wscom/src/main.c
@@ -1,15 +1,28 @@
 #include "luos_engine.h"
 #include "pipe.h"
 #include "gate.h"
+#include <pthread.h>
+
+void *Gate_Pipe_LoopThread(void *vargp)
+{
+    while (1)
+    {
+        Pipe_Loop();
+        Gate_Loop();
+    }
+    return NULL;
+}
+
 int main(void)
 {
     Luos_Init();
     Pipe_Init();
     Gate_Init();
+    // Create a thread to convert messages into Json and steam them using Websocket
+    pthread_t thread_id;
+    pthread_create(&thread_id, NULL, Gate_Pipe_LoopThread, NULL);
     while (1)
     {
         Luos_Loop();
-        Pipe_Loop();
-        Gate_Loop();
     }
 }
