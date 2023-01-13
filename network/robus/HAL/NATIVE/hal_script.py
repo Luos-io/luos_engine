a1fa9c66d98aa5b934bdf09713d6544930f34cc4
JeromeGalan <jerome.galan@luos.io>
2023-01-13 10:14:00 +0100

    Download mongoose only one time even if it's used by multiple libs.

--- network/robus/HAL/NATIVE/hal_script.py
+++ network/robus/HAL/NATIVE/hal_script.py
@@ -3,15 +3,19 @@ import os
 from os import path
 from os.path import realpath
 import click
+import time
 Import("env")
 
+global_env = DefaultEnvironment()
 
-if (not path.exists("mongoose")):
-    env.Execute(
-        "git clone --depth 1 --branch 7.8 https://github.com/cesanta/mongoose.git")
-env.Append(CPPPATH=[realpath("mongoose/")])
+if not "MONGOOSE" in global_env:
+    if (not path.exists("mongoose")):
+        env.Execute(
+            "git clone --depth 1 --branch 7.8 https://github.com/cesanta/mongoose.git")
+    global_env["MONGOOSE"] = realpath("mongoose/")
+env.Append(CPPPATH=[global_env["MONGOOSE"]])
 env.Append(
-    SRC_FILTER=["+<../../../network/robus/HAL/NATIVE/mongoose/mongoose.c>"])
+    SRC_FILTER=["+<"+global_env["MONGOOSE"]+"/mongoose.c>"])
 env.Append(CPPDEFINES=["MG_ENABLE_LINES=1"])
 if os.name == 'nt':
     env.Append(CPPDEFINES=["_POSIX_C_SOURCE=200000L"])
