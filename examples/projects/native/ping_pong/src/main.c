745d55a29f92ce6912e064589d632ef8a6c49b72
Nicolas Rabault <nicolas.rabault@luos.io>
2023-01-27 19:50:22 +0100

    improvement

--- examples/projects/native/ping_pong/src/main.c
+++ examples/projects/native/ping_pong/src/main.c
@@ -1,13 +1,24 @@
 #include "luos_engine.h"
 #include "ping_pong.h"
+#include <pthread.h>
+
+void *PingPong_LoopThread(void *vargp)
+{
+    while (1)
+    {
+        PingPong_Loop();
+    }
+    return NULL;
+}
 
 int main(void)
 {
     Luos_Init();
     PingPong_Init();
+    pthread_t thread_id;
+    pthread_create(&thread_id, NULL, PingPong_LoopThread, NULL);
     while (1)
     {
         Luos_Loop();
-        PingPong_Loop();
     }
 }
