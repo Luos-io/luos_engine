eaf4bd6a1ad76aea5fc0e820c062304ff07bdbc1
Nicolas Rabault <nicolas.rabault@luos.io>
2023-02-07 16:38:18 +0100

    Fix unit test mock HAL

--- test/_resources/Mock/robus_hal.c
+++ test/_resources/Mock/robus_hal.c
@@ -96,6 +96,8 @@ void RobusHAL_SetRxState(uint8_t Enable)
  ******************************************************************************/
 void RobusHAL_ComTransmit(uint8_t *data, uint16_t size)
 {
+    // We consider this information sent
+    Recep_Timeout();
 }
 
 /******************************************************************************
@@ -114,6 +116,7 @@ void RobusHAL_SetRxDetecPin(uint8_t Enable)
  ******************************************************************************/
 uint8_t RobusHAL_GetTxLockState(void)
 {
+    return 0;
 }
 
 /******************************************************************************
