48d530e31c9473a76c991c63c9fa7bc41d414b7d
Nicolas Rabault <nicolas.rabault@luos.io>
2023-03-23 12:22:33 +0100

    Add an error in case of inclusion of luos.h

--- engine/core/inc/luos.h
+++ engine/core/inc/luos.h
@@ -3,4 +3,6 @@
  * @brief Retrocompatibility file to include luos_engine.h instead of luos.h
  * @author Luos
  ******************************************************************************/
+
+#error 'luos.h' inclusion is deprecated since luos_engine@2.3.1, replace it by '#include "luos_engine.h"', see: www.github.com/Luos-io/luos_engine/commit/d3cc1618956907fc5a657ef07535ad9d5caa4f57 for more information.
 #include "luos_engine.h"
--- examples/projects/Arduino/bldc_simple_foc/src/SimpleFOC.cpp
+++ examples/projects/Arduino/bldc_simple_foc/src/SimpleFOC.cpp
@@ -6,7 +6,7 @@ extern "C"
 {
 #endif
 
-#include <luos.h>
+#include "luos_engine.h"
 
 #ifdef __cplusplus
 }
