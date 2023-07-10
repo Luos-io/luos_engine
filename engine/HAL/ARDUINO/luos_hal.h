8c7a6de3e00b9f471d0d5b6400aae83c94f3ed4f
Nicolas Rabault <nicolas.rabault@luos.io>
2023-07-10 16:52:31 +0200

    Add a Pure Arduino Luos HAL

---
+++ engine/HAL/ARDUINO/luos_hal.h
@@ -0,0 +1,49 @@
+/******************************************************************************
+ * @file luosHAL
+ * @brief Luos Hardware Abstration Layer. Describe Low layer fonction
+ * @MCU Family ATSAMD21
+ * @author Luos
+ * @version 0.0.0
+ ******************************************************************************/
+#ifndef _LUOSHAL_H_
+#define _LUOSHAL_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "luos_hal_config.h"
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+#define _CRITICAL
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Function
+ ******************************************************************************/
+void LuosHAL_Init(void);
+void LuosHAL_SetIrqState(bool Enable);
+uint32_t LuosHAL_GetSystick(void);
+
+// bootloader functions
+void LuosHAL_SetMode(uint8_t mode);
+void LuosHAL_Reboot(void);
+void LuosHAL_SaveNodeID(uint16_t);
+
+#ifdef BOOTLOADER_CONFIG
+void LuosHAL_DeInit(void);
+void LuosHAL_JumpToApp(uint32_t);
+uint8_t LuosHAL_GetMode(void);
+uint16_t LuosHAL_GetNodeID(void);
+void LuosHAL_ProgramFlash(uint32_t, uint8_t, uint16_t, uint8_t *);
+#endif
+
+// timestamp functions
+uint64_t LuosHAL_GetTimestamp(void);
+void LuosHAL_StartTimestamp(void);
+void LuosHAL_StopTimestamp(void);
+
+#endif /* _LUOSHAL_H_ */
---
+++ engine/HAL/ARDUINO/luos_hal_config.h
@@ -0,0 +1,38 @@
+/******************************************************************************
+ * @file luosHAL_Config
+ * @brief This file allow you to configure LuosHAL according to your design
+ *        this is the default configuration created by Luos team for this MCU Family
+ *        Do not modify this file if you want to ovewrite change define in you project
+ * @MCU Family ATSAMD21
+ * @author Luos
+ * @version 0.0.0
+ ******************************************************************************/
+#ifndef _LUOSHAL_CONFIG_H_
+#define _LUOSHAL_CONFIG_H_
+
+#include <Arduino.h>
+
+#endif
+
+/*******************************************************************************
+ * DEFINE THREAD MUTEX LOCKING AND UNLOCKING FUNCTIONS
+ ******************************************************************************/
+#ifndef MSGALLOC_MUTEX_LOCK
+#define MSGALLOC_MUTEX_LOCK
+#endif
+#ifndef MSGALLOC_MUTEX_UNLOCK
+#define MSGALLOC_MUTEX_UNLOCK
+#endif
+
+#ifndef LUOS_MUTEX_LOCK
+#define LUOS_MUTEX_LOCK
+#endif
+#ifndef LUOS_MUTEX_UNLOCK
+#define LUOS_MUTEX_UNLOCK
+#endif
+
+/*******************************************************************************
+ * BOOTLOADER CONFIG
+ ******************************************************************************/
+
+#endif /* _LUOSHAL_CONFIG_H_ */
