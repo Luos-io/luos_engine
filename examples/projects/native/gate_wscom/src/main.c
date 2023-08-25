288875b0f2168fb7d2f2c55d965b592ac735a8a0
Nicolas Rabault <nicolas.rabault@luos.io>
2023-08-25 12:50:21 +0200

    Add a trace print on Gate segfault to catch it one day perhaps.

--- examples/projects/native/gate_wscom/src/main.c
+++ examples/projects/native/gate_wscom/src/main.c
@@ -4,6 +4,12 @@
 #include "gate.h"
 #include <pthread.h>
 
+#include <execinfo.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 void *Gate_Pipe_LoopThread(void *vargp)
 {
     while (1)
@@ -14,8 +20,23 @@ void *Gate_Pipe_LoopThread(void *vargp)
     return NULL;
 }
 
+void handler(int sig)
+{
+    void *array[10];
+    size_t size;
+
+    // get void*'s for all entries on the stack
+    size = backtrace(array, 10);
+
+    // print out all the frames to stderr
+    fprintf(stderr, "Error: signal %d:\n", sig);
+    backtrace_symbols_fd(array, size, STDERR_FILENO);
+    exit(1);
+}
+
 int main(void)
 {
+    signal(SIGSEGV, handler); // install our handler
     Luos_Init();
     Serial_Init();
     Pipe_Init();
