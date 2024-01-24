aaebbec95f054cbe20b90a4aa15c7c0df9633593
Nicolas Rabault <nicolas.rabault@luos.io>
2024-01-24 16:23:07 +0100

    Add a custom Gate example

---
+++ examples/projects/product/custom_gate/README.md
@@ -0,0 +1,22 @@
+<a href="https://luos.io"><img src="https://uploads-ssl.webflow.com/601a78a2b5d030260a40b7ad/603e0cc45afbb50963aa85f2_Gif%20noir%20rect.gif" alt="Luos logo" title="Luos" align="right" height="100" /></a>
+
+![](https://github.com/Luos-io/luos_engine/actions/workflows/build.yml/badge.svg)
+[![](https://img.shields.io/github/license/Luos-io/luos_engine)](https://github.com/Luos-io/luos_engine/blob/master/LICENSE)
+
+[![](https://img.shields.io/badge/Luos-Documentation-34A3B4)](https://www.luos.io/docs/)
+[![PlatformIO Registry](https://badges.registry.platformio.org/packages/luos/library/luos_engine.svg)](https://registry.platformio.org/libraries/luos_engine/luos_engine)
+
+[![](https://img.shields.io/discord/902486791658041364?label=Discord&logo=discord&style=social)](http://bit.ly/JoinLuosDiscord)
+[![](https://img.shields.io/badge/LinkedIn-Share-0077B5?style=social&logo=linkedin)](https://www.linkedin.com/sharing/share-offsite/?url=https%3A%2F%2Fgithub.com%2Fluos-io)
+
+# Custom Gate project example :bulb:
+
+This project demonstrate how to make a custom gate with custom Json conversion.
+
+## How to compile the code :computer:
+
+1.  Download and install [Platformio](https://platformio.org/platformio-ide)
+2.  Open this folder into Platformio
+3.  Build (Platformio will do the rest)
+
+## Don't hesitate to read [our documentation](https://www.luos.io/docs/), or to post your questions/issues on the [Luos' Discord](http://bit.ly/JoinLuosDiscord). :books:
---
+++ examples/projects/product/custom_gate/src/custom_json_conversion.c
@@ -0,0 +1,98 @@
+#include "custom-json.h"
+#include "product_config.h"
+#include <stdio.h>
+
+// This function are called by the gate conversion functions in case of an unknown type.
+// This functions allow you to easily add and manage your custom type and commands.
+
+// This function is called by the gate to convert a service type into a string.
+// This is typically used in the end of detection to create a Json representing the device routing table.
+// The name of the type will be used by pyluos to create the right object.
+const char *Convert_CustomStringFromType(luos_type_t type)
+{
+    if (type == POINT_2D)
+    {
+        return "point_2D";
+    }
+    return NULL;
+}
+
+// This function is called by the gate to convert a piece of Json into a message.
+// This is typically used when a Json is received by the gate with an unknown property.
+// You can use it to compose your own message out of the Json data and send it to the right service.
+void Convert_CustomJsonToMsg(service_t *service, uint16_t target_id, char *property, const json_t *jobj, char *json_str)
+{
+    msg_t msg;
+    msg.header.target_mode = IDACK;
+    msg.header.target      = target_id;
+    // Target linear position 2D
+    if (property && !strcmp(property, "linear_pos_2D"))
+    {
+        // Check the size of the array. If we have more than one data, this mean that this data is a binary size of a trajectory. If we have 2 data, this is an unique point.
+        json_t const *item = json_getChild(jobj);
+        if (json_getSibling(item) != NULL)
+        {
+            // We only have one point in this data
+            pos_2d_t pos;
+            pos.x = (uint16_t)json_getInteger(item);
+            item  = json_getSibling(item);
+            pos.y = (uint16_t)json_getInteger(item);
+            // Create the message
+            msg.header.cmd  = LINEAR_POSITION_2D;
+            msg.header.size = sizeof(pos_2d_t);
+            memcpy(msg.data, &pos, sizeof(pos_2d_t));
+            // Send the message
+            Luos_SendMsg(service, &msg);
+        }
+        else
+        {
+            int i = 0;
+            // This is a binary
+            int size = (int)json_getInteger(item);
+            // Find the first \r of the current json_str
+            for (i = 0; i < GATE_BUFF_SIZE; i++)
+            {
+                if (json_str[i] == '\n')
+                {
+                    i++;
+                    break;
+                }
+            }
+            if (i < GATE_BUFF_SIZE - 1)
+            {
+                // Create the message
+                msg.header.cmd = LINEAR_POSITION_2D;
+                Luos_SendData(service, &msg, &json_str[i], (unsigned int)size);
+            }
+        }
+        return;
+    }
+    if (property && !strcmp(property, "buffer_mode"))
+    {
+        msg.data[0]     = (char)json_getInteger(jobj);
+        msg.header.cmd  = BUFFER_MODE;
+        msg.header.size = sizeof(char);
+        Luos_SendMsg(service, &msg);
+        return;
+    }
+}
+
+// This function is called by the gate to convert a message into a piece of Json.
+// This is typically used when a message is received by the gate with an unknown command.
+// You can use it to compose your own piece of Json out of the message data.
+void Convert_CustomMsgToJson(msg_t *msg, char *data)
+{
+    if (msg->header.cmd == LINEAR_POSITION_2D)
+    {
+        // This is our custom message, so we can convert it to JSON
+        // In this case we will don't need it but I did the code for the sake of the example.
+        if (msg->header.size == sizeof(pos_2d_t))
+        {
+            // Size ok, now fill the struct from msg data
+            pos_2d_t pos;
+            memcpy(&pos, msg->data, msg->header.size);
+            // create the Json content
+            sprintf(data, "\"linear_pos_2D\":[%2d,%2d],", pos.x, pos.y);
+        }
+    }
+}
