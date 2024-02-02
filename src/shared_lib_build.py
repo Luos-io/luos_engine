9fe014a2f4203c8d6f56e9348c2da4f327df9e28
Nicolas Rabault <nicolas.rabault@luos.io>
2024-02-02 17:26:24 +0100

    add -fpic flag

--- src/shared_lib_build.py
+++ src/shared_lib_build.py
@@ -24,18 +24,19 @@ def shared_lib(source, target, env):
     if libPath is not None:
         # Convert the luos_engine.a archive to a shared library
         if (pf.system() == 'Windows'):
-            env.Execute("gcc -O2 -shared -o $BUILD_DIR/libluos_engine.dll " + libPath)
+            env.Execute("gcc -shared -fPIC  -o $BUILD_DIR/libluos_engine.dll " + libPath)
             click.secho("* Luos engine shared library available in " + str(env.subst("$BUILD_DIR")) + "/libluos_engine.dll .", fg="green")
         elif (pf.system() == 'Linux'):
-            env.Execute("gcc -O2 -shared -o $BUILD_DIR/libluos_engine.so " + libPath)
+            env.Execute("gcc -shared -fPIC -o $BUILD_DIR/libluos_engine.so " + libPath)
             click.secho("* Luos engine shared library available in " + str(env.subst("$BUILD_DIR")) + "/libluos_engine.so .", fg="green")
         elif (pf.system() == 'Darwin'):
             for networklib in networklibs:
-                env.Execute("gcc -O2 -shared -o $BUILD_DIR/" + os.path.basename(networklib)[0:-10] + "_luos_engine.dylib " + libPath + " " + networklib)
-
+                env.Execute("gcc -v -shared -fPIC -o $BUILD_DIR/" + os.path.basename(networklib)[0:-10] + "_luos_engine.dylib " + libPath + " " + networklib)
             click.secho("\n")
             click.secho("Luos engine shared libraries available in " + str(env.subst("$BUILD_DIR")) + "/ :", underline=True)
             for networklib in networklibs:
                 click.secho("\t* " + os.path.basename(networklib)[0:-10] + "_luos_engine.dylib ", fg="green")
 
 env.AddPostAction("$PROGPATH", shared_lib)
+env.Append(LINKFLAGS=["-fPIC"])
+env.Append(BUILD_FLAGS=["-fPIC"])
